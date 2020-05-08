1779e4faaf242e98a75c3d600824e9afc29306ac
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-05-08 15:13:24 +0900

    Add VSCode debug setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -106,6 +106,7 @@ export default {
     extend(config, ctx) {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
+        config.devtool = 'inline-cheap-module-source-map'
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
