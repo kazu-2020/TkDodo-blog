e0102e2bf2f82a3b7089a324e92fc8c1e3bda1b3
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-05-08 15:41:45 +0900

    Fix api base url

--- env.production.js
+++ env.production.js
@@ -1,4 +1,4 @@
 module.exports = {
   apiBaseUrl:
-    'https://dw4i6rvnz8.execute-api.ap-northeast-1.amazonaws.com/dev/',
+    'https://estlwxyv3j.execute-api.ap-northeast-1.amazonaws.com/prod/playlists',
 }
