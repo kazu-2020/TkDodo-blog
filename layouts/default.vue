43412dad43348345900c510bd876a244940f08bf
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-05-11 15:03:16 +0900

    Add playlist new dialog

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,12 +10,55 @@
           </v-btn>
         </template>
         <v-list>
-          <v-list-item
-            v-for="(item, index) in playlistItems"
-            :key="index"
-            :to="item.to"
-          >
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <v-list-item :to="'/'">
+            <v-list-item-title>一覧</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-dialog v-model="dialog" persistent max-width="600px">
+              <template v-slot:activator="{ on }">
+                <v-list-item-title class="playlist_new" v-on="on">
+                  新規作成
+                </v-list-item-title>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">プレイリストを作成</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="6" sm="12" md="5">
+                        <v-card class="pa-2" tile>
+                          <v-list-item-avatar tile size="200" color="grey" />
+                        </v-card>
+                      </v-col>
+                      <v-col cols="6" sm="12" md="6">
+                        <v-text-field label="プレイリスト名 - Name" required />
+                        <v-text-field
+                          label="ふりがな - Detailed Series Name Ruby"
+                          required
+                        />
+                        <v-textarea label="説明 - Description" auto-grow />
+                      </v-col>
+                    </v-row>
+                    <v-row justify="center" align-content="center">
+                      <v-col cols="5">
+                        <v-btn depressed color="orange" large>
+                          この内容で新規作成する
+                        </v-btn>
+                        <small>* 内容は後で変更できます</small>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer />
+                  <v-btn color="white darken-1" text @click="dialog = false">
+                    <v-icon>mdi-close</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </v-list-item>
         </v-list>
       </v-menu>
@@ -28,7 +71,7 @@
         </template>
         <v-list>
           <v-list-item
-            v-for="(item, index) in deckItems"
+            v-for="(item, index) in playlisticleItems"
             :key="index"
             :to="item.to"
           >
@@ -56,17 +99,7 @@ export default {
       clipped: false,
       drawer: true,
       fixed: false,
-      playlistItems: [
-        {
-          title: '一覧',
-          to: '/',
-        },
-        {
-          title: '新規作成',
-          to: '/playlists/new',
-        },
-      ],
-      deckItems: [
+      playlisticleItems: [
         {
           title: '一覧',
           to: '/playlisticles',
@@ -80,6 +113,7 @@ export default {
       right: false,
       rightDrawer: false,
       title: 'PLAYLIST（仮）',
+      dialog: false,
     }
   },
 }
