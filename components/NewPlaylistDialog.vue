e6ee0c00e41d6c16de359c4422c108aa9f449a5c
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-05-11 15:10:53 +0900

    Move dialog from layout to component

---
+++ components/NewPlaylistDialog.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on }">
+      <v-list-item-title class="playlist_new" v-on="on">
+        新規作成
+      </v-list-item-title>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">プレイリストを作成</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="6" sm="12" md="5">
+              <v-card class="pa-2" tile>
+                <v-list-item-avatar tile size="200" color="grey" />
+              </v-card>
+            </v-col>
+            <v-col cols="6" sm="12" md="6">
+              <v-text-field label="プレイリスト名 - Name" required />
+              <v-text-field
+                label="ふりがな - Detailed Series Name Ruby"
+                required
+              />
+              <v-textarea label="説明 - Description" auto-grow />
+            </v-col>
+          </v-row>
+          <v-row justify="center" align-content="center">
+            <v-col cols="5">
+              <v-btn depressed color="orange" large>
+                この内容で新規作成する
+              </v-btn>
+              <small>* 内容は後で変更できます</small>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="white darken-1" text @click="dialog = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.playlist_new {
+  cursor: pointer;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -14,51 +14,7 @@
             <v-list-item-title>一覧</v-list-item-title>
           </v-list-item>
           <v-list-item>
-            <v-dialog v-model="dialog" persistent max-width="600px">
-              <template v-slot:activator="{ on }">
-                <v-list-item-title class="playlist_new" v-on="on">
-                  新規作成
-                </v-list-item-title>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">プレイリストを作成</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="6" sm="12" md="5">
-                        <v-card class="pa-2" tile>
-                          <v-list-item-avatar tile size="200" color="grey" />
-                        </v-card>
-                      </v-col>
-                      <v-col cols="6" sm="12" md="6">
-                        <v-text-field label="プレイリスト名 - Name" required />
-                        <v-text-field
-                          label="ふりがな - Detailed Series Name Ruby"
-                          required
-                        />
-                        <v-textarea label="説明 - Description" auto-grow />
-                      </v-col>
-                    </v-row>
-                    <v-row justify="center" align-content="center">
-                      <v-col cols="5">
-                        <v-btn depressed color="orange" large>
-                          この内容で新規作成する
-                        </v-btn>
-                        <small>* 内容は後で変更できます</small>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn color="white darken-1" text @click="dialog = false">
-                    <v-icon>mdi-close</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+            <new-playlist-dialog />
           </v-list-item>
         </v-list>
       </v-menu>
@@ -93,7 +49,12 @@
 </template>
 
 <script>
+import NewPlaylistDialog from '../components/NewPlaylistDialog'
+
 export default {
+  components: {
+    NewPlaylistDialog,
+  },
   data() {
     return {
       clipped: false,
