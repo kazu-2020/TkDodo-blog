055c5aa62df34a419fcade901911bb25b8ae15d2
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-05-15 13:41:40 +0900

    Introduce core editor.js

---
+++ pages/playlisticles/sandbox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hoge">
+    <div id="codex-editor" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import EditorJS from '@editorjs/editorjs'
+
+@Component
+export default class PlaylisticleSandbox extends Vue {
+  editor?: object = {}
+
+  doEditor() {
+    this.editor = new EditorJS({
+      holder: 'codex-editor',
+    })
+  }
+
+  mounted() {
+    this.doEditor()
+  }
+}
+</script>
