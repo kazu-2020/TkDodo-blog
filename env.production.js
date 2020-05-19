9df21927eaa990c4667c2bc6dc8ad151ab5eaec1
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-05-19 18:32:29 +0900

    Fix api base url for production & staging

--- env.production.js
+++ env.production.js
@@ -1,4 +1,4 @@
 module.exports = {
   apiBaseUrl:
-    'https://estlwxyv3j.execute-api.ap-northeast-1.amazonaws.com/prod/',
+    'https://hi0ovay4z4.execute-api.ap-northeast-1.amazonaws.com/prod/',
 }
