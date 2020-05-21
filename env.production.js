28aab1a673de22b4ec4c4c3196ec8c452f6bf513
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-05-21 14:45:29 +0900

    Fix backend url

--- env.production.js
+++ env.production.js
@@ -1,4 +1,4 @@
 module.exports = {
   apiBaseUrl:
-    'https://hi0ovay4z4.execute-api.ap-northeast-1.amazonaws.com/prod/',
+    'https://nqcqbk3e33.execute-api.ap-northeast-1.amazonaws.com/prod/',
 }
