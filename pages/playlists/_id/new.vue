f80cf8869adaa61658142089ec2d4d7cc50152b2
norikt <norikt@gmail.com>
2020-06-10 15:14:16 +0900

    色のフォームを作成中

--- pages/playlists/_id/new.vue
+++ pages/playlists/_id/new.vue
@@ -179,6 +179,36 @@
                 <v-sheet width="40" height="40" elevation="4" color="white" />
               </v-col>
             </v-row>
+            <v-row justify="start" align="center">
+              <v-col class="shrink" style="min-width: 220px;">
+                <v-text-field
+                  v-model="color"
+                  v-mask="mask"
+                  hide-details
+                  class="ma-0 pa-0"
+                  solo
+                >
+                  <template v-slot:append>
+                    <v-menu
+                      v-model="menu"
+                      top
+                      nudge-bottom="105"
+                      nudge-left="16"
+                      :close-on-content-click="false"
+                    >
+                      <template v-slot:activator="{ on }">
+                        <div :style="swatchStyle" v-on="on" />
+                      </template>
+                      <v-card>
+                        <v-card-text class="pa-0">
+                          <v-color-picker v-model="color" flat />
+                        </v-card-text>
+                      </v-card>
+                    </v-menu>
+                  </template>
+                </v-text-field>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
 
@@ -436,7 +466,23 @@ export default Vue.extend({
       { value: '096', text: '芸術' },
       { value: '110', text: '福祉全般' },
     ],
+    color: '#FFFFFF',
+    mask: '!#XXXXXXXX',
+    menu: false,
   }),
+  computed: {
+    swatchStyle() {
+      const { color, menu } = this
+      return {
+        backgroundColor: color,
+        cursor: 'pointer',
+        height: '30px',
+        width: '30px',
+        borderRadius: menu ? '50%' : '4px',
+        transition: 'border-radius 200ms ease-in-out',
+      }
+    },
+  },
   methods: {
     addDetailedTimeTable() {
       const detailedTimeTables: Array<DetailedTimeTable> = this.playlist
