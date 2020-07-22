8c164f609393eec9c42a904c625e4fc63379a553
norikt <norikt@gmail.com>
2020-07-22 09:52:53 +0900

    Fix submodule name

--- components/NewPlaylistDialog.vue
+++ components/NewPlaylistDialog.vue
@@ -128,7 +128,7 @@ export default Vue.extend({
 
       if (this.valid) {
         this.loadingDialog = true
-        this.$accessor.playlist.createPlaylists({
+        this.$accessor.playlists.createPlaylists({
           playlist: {
             name: this.name,
             description: this.description,
