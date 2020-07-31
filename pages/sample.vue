e93321af0e09db5128fec920e51319f7ac0b15d2
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-07-31 10:51:26 +0900

    Sentry を導入

---
+++ pages/sample.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout column>
+    <v-row>
+      <v-col cols="12">
+        <div class="title mb-4">
+          必ずエラーになるページ
+        </div>
+      </v-col>
+      <v-col cols="12">
+        <v-alert outlined type="warning">
+          Sentry 試験用
+        </v-alert>
+      </v-col>
+    </v-row>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'SamplePage',
+  mounted() {
+    throw new Error('Always wrong!!')
+  },
+})
+</script>
