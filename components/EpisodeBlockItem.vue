fba7b044fa5ed3f12dfd975b5973fd7064aff37d
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-08-04 11:43:51 +0900

    Event, HowTwo, Recipe もエディタに挿入できるようにした

--- components/EpisodeBlockItem.vue
+++ components/EpisodeBlockItem.vue
@@ -33,7 +33,7 @@
           <v-list-item
             v-for="(otherTypeItem, i) in otherTypeItems"
             :key="i"
-            @click="hoge"
+            @click="addOtherTypeBlock(otherTypeItem.type)"
           >
             <v-list-item-icon>
               <v-icon v-text="otherTypeItem.icon" />
@@ -50,6 +50,9 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import sampleEventData from '~/assets/json/event_LR3P5RJ389.json'
+import sampleHowToData from '~/assets/json/howTo_G9218G45GJ.json'
+import sampleRecipeData from '~/assets/json/recipe.json'
 
 interface DataType {
   show: boolean
@@ -72,14 +75,17 @@ export default Vue.extend({
         {
           text: '「新ごぼうで和おかず 新ごぼうのフレッシュきんぴら」',
           icon: 'mdi-silverware-fork-knife',
+          type: 'recipe',
         },
         {
           text: '2020年5月13日 - あわ怪人をやっつけよう',
           icon: 'mdi-calendar-month',
+          type: 'event',
         },
         {
           text: 'アイスが溶けそう～！myボトルマーカーの作り方',
           icon: 'mdi-hammer-wrench',
+          type: 'howto',
         },
       ],
     }
@@ -106,6 +112,28 @@ export default Vue.extend({
           episode: this.episode,
         },
       })
+    },
+    addOtherTypeBlock(type: string) {
+      switch (type) {
+        case 'recipe':
+          this.$emit('add-block', {
+            type: 'recipe',
+            data: sampleRecipeData,
+          })
+          break
+        case 'event':
+          this.$emit('add-block', {
+            type: 'tvEvent',
+            data: sampleEventData,
+          })
+          break
+        case 'howto':
+          this.$emit('add-block', {
+            type: 'howTo',
+            data: sampleHowToData,
+          })
+          break
+      }
     },
   },
 })
