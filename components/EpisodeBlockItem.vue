360759e8232a15717aee53c8cfb31e0060741455
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-08-04 11:16:04 +0900

    NArticle 用のSandbox を追加

---
+++ components/EpisodeBlockItem.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-card>
+    <div class="d-flex flex-no-wrap justify-space-between">
+      <div>
+        <v-card-title class="text-md-body-1">
+          {{ episode.name }}
+        </v-card-title>
+        <v-card-subtitle>
+          {{ truncate(episode.description, 40) }}
+        </v-card-subtitle>
+      </div>
+      <v-avatar class="ma-3" size="80" tile>
+        <v-img :src="eyecatchUrl(episode.eyecatch)" />
+      </v-avatar>
+    </div>
+    <v-card-actions>
+      <v-btn text @click="addEpisodeBlock">
+        エピソード追加
+      </v-btn>
+      <v-spacer />
+      <v-btn text @click="show = !show">
+        <span>その他</span>
+        <v-icon>
+          {{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
+        </v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider />
+        <v-list subheader>
+          <v-subheader>別タイプのアイテム</v-subheader>
+          <v-list-item
+            v-for="(otherTypeItem, i) in otherTypeItems"
+            :key="i"
+            @click="hoge"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="otherTypeItem.icon" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="otherTypeItem.text" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface DataType {
+  show: boolean
+  otherTypeItems: Array<Object>
+}
+
+export default Vue.extend({
+  name: 'EpisodeBlockItem',
+  props: {
+    episode: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+  data(): DataType {
+    return {
+      show: false,
+      otherTypeItems: [
+        {
+          text: '「新ごぼうで和おかず 新ごぼうのフレッシュきんぴら」',
+          icon: 'mdi-silverware-fork-knife',
+        },
+        {
+          text: '2020年5月13日 - あわ怪人をやっつけよう',
+          icon: 'mdi-calendar-month',
+        },
+        {
+          text: 'アイスが溶けそう～！myボトルマーカーの作り方',
+          icon: 'mdi-hammer-wrench',
+        },
+      ],
+    }
+  },
+  methods: {
+    truncate(str: string, len: number) {
+      return str.length <= len ? str : str.substr(0, len) + '...'
+    },
+    eyecatchUrl(eyecatch: any) {
+      if (eyecatch !== undefined) {
+        return eyecatch.medium.url
+      } else {
+        return ''
+      }
+    },
+    hoge() {
+      // do nothing
+    },
+    addEpisodeBlock() {
+      this.$emit('add-block', {
+        type: 'episode',
+        data: {
+          url: this.episode.url,
+          episode: this.episode,
+        },
+      })
+    },
+  },
+})
+</script>
---
+++ pages/playlisticles/sandbox2.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="editor-sandbox">
+    <div class="title">
+      Playlisticle Sandbox
+      <v-btn
+        class="ml-2"
+        @click.stop="isShowPreviewDrawer = !isShowPreviewDrawer"
+      >
+        <v-icon class="mr-2">
+          mdi-eye
+        </v-icon>
+        Preview
+      </v-btn>
+    </div>
+    <v-divider class="ma-2" />
+    <v-layout column>
+      <v-row>
+        <v-col xs="12" sm="12" md="8" lg="8">
+          <editable-section
+            key="sandbox2"
+            section-id="sandbox2"
+            :initial-data="article"
+            :episode-block-id="episodeBlockId"
+            class="mr-8"
+          />
+        </v-col>
+        <v-col xs="12" sm="12" md="4" lg="4" :class="verticalDivider">
+          <div class="title py-2">
+            <span>{{ playlist.name }}</span> のアイテム一覧
+          </div>
+          <episode-block-item
+            v-for="item in playlist.items"
+            :key="item.id"
+            class="my-4"
+            :episode="item"
+            @add-block="addNewBlock"
+          />
+        </v-col>
+      </v-row>
+    </v-layout>
+    <preview-drawer
+      key="time"
+      :is-show-drawer="isShowPreviewDrawer"
+      :preview-data="combineEditorJson()"
+      @current-drawer-state="updatePreviewDrawerState"
+    />
+  </div>
+</template>
+
+<script>
+import EditableSection from '~/components/EditableSection.vue'
+import PreviewDrawer from '~/components/PreviewDrawer.vue'
+import editorBlockMixin from '~/components/mixins/editorBlockMixin'
+import EpisodeBlockItem from '~/components/EpisodeBlockItem.vue'
+
+export default {
+  components: {
+    'editable-section': EditableSection,
+    'preview-drawer': PreviewDrawer,
+    'episode-block-item': EpisodeBlockItem,
+  },
+  mixins: [editorBlockMixin],
+  asyncData({ $axios }) {
+    return $axios.get('/api/playlisticles/sandbox2').then((res) => {
+      return {
+        article: res.data.playlisticle.article,
+        playlist: res.data.playlisticle.playlist,
+      }
+    })
+  },
+  data() {
+    return {
+      isShowPreviewDrawer: false,
+      selectedSection: null,
+      article: {},
+      playlist: {},
+    }
+  },
+  computed: {
+    playlisticle() {
+      return this.$store.state.playlisticles.editingPlaylisticle
+    },
+    verticalDivider() {
+      if (this.$vuetify.theme.dark) {
+        return 'vertical_divider dark'
+      } else {
+        return 'vertical_divider'
+      }
+    },
+    episodeBlockId() {
+      if (this.article?.time) {
+        return this.article.time.toString()
+      } else {
+        return Date.now().toString()
+      }
+    },
+  },
+  methods: {
+    updatePreviewDrawerState(newVal) {
+      this.isShowPreviewDrawer = newVal
+    },
+    combineEditorJson() {
+      const combindedJson = { time: Date.now(), blocks: this.article }
+
+      return combindedJson
+    },
+    addNewBlock(blockData) {
+      this.article.blocks.push(blockData)
+      this.article.time = Date.now()
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.vertical_divider {
+  border-left: 1px solid rgba(0, 0, 0, 0.12);
+
+  &.dark {
+    border-left: 1px solid rgba(255, 255, 255, 0.12);
+  }
+}
+</style>
