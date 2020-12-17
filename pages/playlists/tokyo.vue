d64024e591a80f584def9e7b64bea5a9989624d8
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2020-12-17 11:05:33 +0900

    R5 東京デッキページでもドロワーが出るように修正

--- pages/playlists/tokyo.vue
+++ pages/playlists/tokyo.vue
@@ -28,7 +28,10 @@
     </v-row>
     <v-row>
       <v-col v-for="item in playlists" :key="item.id" cols="12" class="py-2">
-        <playlist-item :playlist="item" @delete-playlist="deletePlaylist" />
+        <playlist-item
+          :playlist="item"
+          @click-playlist-item="clickPlaylistItem"
+        />
       </v-col>
     </v-row>
     <v-row v-if="totalPages > 1">
@@ -44,21 +47,109 @@
         </div>
       </v-col>
     </v-row>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      right
+      width="400"
+      style="position: fixed"
+    >
+      <v-list-item class="pa-2">
+        <v-col cols="12">
+          <basic-information-view :playlist="selectedPlaylist" />
+        </v-col>
+      </v-list-item>
+      <v-list-item
+        v-for="item in selectedPlaylistItems"
+        :key="item.id"
+        class="px-6 episode_list"
+      >
+        <v-list-item-icon class="mr-1 my-1">
+          <v-img
+            :src="eyecatchUrl(item)"
+            lazy-src="https://placehold.jp/50x28.png"
+            width="50"
+            height="28"
+            class="episode-image"
+          />
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title style="font-size: 14px" v-text="item.name" />
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider class="mt-4" />
+      <v-list-item>
+        <div class="article_preview">
+          {{ selectedPlaylistArticle }}
+        </div>
+      </v-list-item>
+      <v-list-item>
+        <v-row justify="center">
+          <v-col cols="4">
+            <v-btn
+              :to="{
+                name: 'playlists-id',
+                params: { id: selectedPlaylistId },
+              }"
+              nuxt
+              depressed
+              color="orange"
+              class="edit_button"
+            >
+              <v-icon left>mdi-pencil</v-icon>
+              編集する
+            </v-btn>
+          </v-col>
+          <v-col cols="4">
+            <v-btn
+              class="delete_button"
+              outlined
+              @click="deleteSelectedPlaylist"
+            >
+              <v-icon left>mdi-delete</v-icon>
+              削除する
+            </v-btn>
+          </v-col>
+          <v-col cols="6">
+            <v-btn
+              :to="{
+                name: 'playlists-id-edit2',
+                params: { id: selectedPlaylistId },
+              }"
+              nuxt
+              depressed
+              color="orange"
+              class="edit_button"
+            >
+              <v-icon left>mdi-flask-round-bottom</v-icon>
+              編集する(試用版)
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-list-item>
+    </v-navigation-drawer>
   </v-layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import BasicInformationView from '~/components/playlists/BasicInformationView.vue'
 import PlaylistItem from '~/components/common/PlaylistItem.vue'
+import { Playlist } from '~/types/playlist'
+import { EpisodeData } from '~/types/episode_data'
 
 interface DataType {
   page: number
   totalVisiblePagination: number
+  drawer: boolean
+  selectedPlaylist: Playlist | undefined
+  selectedPlaylistItems: EpisodeData[]
 }
 
 export default Vue.extend({
   name: 'PlaylistTokyoPage',
-  components: { PlaylistItem },
+  components: { PlaylistItem, BasicInformationView },
   async asyncData({ store }) {
     await store.dispatch('playlists/fetchD5Playlists', { area: '130', page: 1 })
   },
@@ -66,6 +157,9 @@ export default Vue.extend({
     return {
       page: 1,
       totalVisiblePagination: 9,
+      drawer: false,
+      selectedPlaylist: undefined,
+      selectedPlaylistItems: [],
     }
   },
   computed: {
@@ -74,6 +168,12 @@ export default Vue.extend({
     },
     totalPages() {
       return this.$store.state.playlists.pagination.totalPages
+    },
+    selectedPlaylistId(): string {
+      return this.selectedPlaylist ? this.selectedPlaylist.id : ''
+    },
+    selectedPlaylistArticle(): string {
+      return this.selectedPlaylist?.article?.plainBody || ''
     },
   },
   watch: {
@@ -85,15 +185,59 @@ export default Vue.extend({
         })
       },
     },
+    drawer: {
+      handler(newValue) {
+        if (!newValue) {
+          this.selectedPlaylist = undefined
+        }
+      },
+    },
+    selectedPlaylist: {
+      handler() {
+        this.fetchEpisodes()
+      },
+    },
   },
   methods: {
-    deletePlaylist(playlist: any) {
+    deleteSelectedPlaylist(): void {
       this.$store.dispatch('loading/startLoading', {
         success: '削除しました',
         error: '削除失敗しました',
       })
-      this.$store.dispatch('playlists/deletePlaylist', playlist)
+      this.$store.dispatch('playlists/deletePlaylist', this.selectedPlaylist)
+    },
+    clickPlaylistItem(playlist: any) {
+      this.drawer = true
+      this.selectedPlaylistItems = []
+      this.selectedPlaylist = playlist
+    },
+    eyecatchUrl(item: any): string {
+      if (item.eyecatch !== undefined) {
+        return item.eyecatch.medium.url
+      } else {
+        return ''
+      }
+    },
+    fetchEpisodes(): void {
+      if (this.selectedPlaylist === undefined) return
+      if (this.selectedPlaylistItems.length !== 0) return
+
+      this.$axios
+        .get(`/playlists/${this.selectedPlaylist.id}/playlist_items`)
+        .then((res) => {
+          this.selectedPlaylistItems = res.data.items
+        })
     },
   },
 })
 </script>
+
+<style lang="scss" scoped>
+.edit_button {
+  color: white;
+}
+
+.delete_button {
+  color: #4f4f4f;
+}
+</style>
