6e29ba171605e62a6a2698918e25b8eb16d6ab6a
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2021-01-20 16:08:27 +0900

    デッキ名を変更するワンショットジョブを追加

--- backend/app/jobs/decks_job.rb
+++ backend/app/jobs/decks_job.rb
@@ -3,26 +3,16 @@
 class DecksJob < ApplicationJob
   class_timeout 300 # seconds
 
-  def initialize_deck
-    deck = Deck.first
-    deck.is_r5 = true
+  cron '0 8 20 1 ? 2021' # 2021/01/20 17:00(JST) に実行 FIXME: 一時的なバッチ処理、後で消す
+  def rename_deck
+    r5_deck = Deck.find_by(is_r5: true)
+    r5_deck.name = '番組まとめ (東京/r5)'
 
-    deck.save
+    r5_deck.save!
 
-    Deck.create(
-      name: 'おすすめプレイリスト',
-      description: '説明が入ります。',
-      area: '130'
-    )
-  end
-
-  def assign_all_playlist_to_deck
     deck = Deck.find_by(is_r5: false)
-    playlists = Playlist.where('deck_id IS NULL')
+    deck.name = '番組まとめ (東京)'
 
-    playlists.each do |playlist|
-      playlist.deck = deck
-      playlist.save
-    end
+    deck.save!
   end
 end
