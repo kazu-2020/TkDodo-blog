914e3c0215b0e7198aa430affe6f0ad7b412c55b
norikt <norikt@gmail.com>
2021-01-28 10:58:59 +0900

    editor_dataのparserの修正

--- backend/app/jobs/playlist_job.rb
+++ backend/app/jobs/playlist_job.rb
@@ -10,4 +10,12 @@ class PlaylistJob < ApplicationJob
   def recalculate_playlist_item_duration
     PlaylistItem.all.each(&:fetch_data)
   end
+
+  cron '00 3 28 1 ? 2021' # 2021/01/28 12:00に実行 FIXME: 一時的なバッチ処理
+  def tmp_article_bodies_regenerate
+    Playlist.find_each do |pl|
+      pl.set_bodies
+      pl.save!
+    end
+  end
 end
