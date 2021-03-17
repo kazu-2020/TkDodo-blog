b5ba24e32e139852beb5ca10f705352854c00d02
norikt <norikt@gmail.com>
2021-03-17 17:28:24 +0900

    responseを修正

--- backend/app/controllers/oembed_controller.rb
+++ backend/app/controllers/oembed_controller.rb
@@ -15,20 +15,21 @@ class OembedController < ApplicationController
   def dummy_response
     url = params[:url]
     series_id = url[/https?:\/\/.*nhk.jp.*\/ts\/([A-Z1-9]{10})/, 1] || 'WV5PLY8R43'
+    episode_id = url[/https?:\/\/.*nhk.jp.*\/te\/([A-Z1-9]{10})/, 1] || 'P25ZGZRV29'
 
     {
-      version: '1.0',
-      width: '100%',
+      version: "1.0",
+      width: "100%",
       height: 231,
-      type: 'rich',
-      provider_name: 'NHK',
-      provider_url: 'https://www.nhk.jp',
-      url: "https://www.nhk.jp/p/ts/#{series_id}/",
-      title: "TVSeries##{series_id} - episode",
+      type: "rich",
+      provider_name: "NHK",
+      provider_url: "https://www.nhk.jp",
+      url: "https://www.nhk.jp/p/ts/#{series_id}{/episode/te/#{episode_id}/",
+      title: "TVSeries##{series_id} - Episode##{episode_id}",
       thumbnail_width: 640,
       thumbnail_height: 360,
       thumbnail_url: "http://placehold.jp/640x360.png",
-      html: "<iframe width='100%' height='231' src='https://dev-embed.nr.nhk.jp/p/ts/#{series_id}/' frameborder='0'></iframe>"
+      html: "<iframe width=\"100%\" height=\"231\" src=\"https://dev-embed.nr.nhk.jp/p/ts/#{series_id}/episode/te/#{episode_id}/\" frameborder=\"0\"></iframe>"
     }
   end
 end
