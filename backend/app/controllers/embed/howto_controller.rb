fc0619041f0ee6101ed08da4e9d0427add6a91f9
norikt <norikt@gmail.com>
2021-04-06 12:30:09 +0900

    oEmbedのレスポンスを修正

--- backend/app/controllers/embed/howto_controller.rb
+++ backend/app/controllers/embed/howto_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Embed::HowtoController < EmbedController
-  skip_before_action :set_content_type
-
   def show
     episode_id = params[:episode_id]
     res = DlabApiClient.new(api_endpoint: 'https://api.nr.nhk.jp').episode_bundle(type: 'tv', episode_id: episode_id)
