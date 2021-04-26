0ce83f66a8016d926d7a3a8b4093b5aa321bf35c
norikt <norikt@gmail.com>
2021-04-26 10:16:46 +0900

    エピソード画像が取得できない場合を考慮

--- backend/app/helpers/embed/episodes_helper.rb
+++ backend/app/helpers/embed/episodes_helper.rb
@@ -2,6 +2,18 @@
 
 module Embed::EpisodesHelper
   def episode_image_src
-    @episode_data.dig(:eyecatch, :medium, :url) || @episode_data[:keyvisuals].first.dig(:small, :url)
+    eyecatch_image_url || keyvisual_url || '//via.placeholder.com/640x360'
+  end
+
+  def eyecatch_image_url
+    @episode_data.dig(:eyecatch, :medium,
+                      :url) || @episode_data.dig(:eyecatch, :tver, :url) || @episode_data.dig(:eyecatch, :main, :url)
+  end
+
+  def keyvisual_url
+    kv = @episode_data[:keyvisuals].first
+    return nil if kv.blank?
+
+    kv.dig(:small, :url) || kv.dig(:large, :url) || kv.dig(:xlarge, :url)
   end
 end
