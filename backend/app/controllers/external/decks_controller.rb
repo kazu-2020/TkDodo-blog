a8ef0705ef92186f03dc4ad37961ea8e5be75afa
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2021-04-28 16:55:43 +0900

    /t/ndeck/recommend のパスで theme_genre_code による絞り込みを許可する

--- backend/app/controllers/external/decks_controller.rb
+++ backend/app/controllers/external/decks_controller.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class External::DecksController < ApplicationController
-  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def show
     @request_url = request.url
     @area = params[:area]
     is_r5 = !(params[:deck_id] =~ /r5/).nil?
     @deck = Deck.find_by(area: @area, is_r5: is_r5)
+    @playlists =
+      if params[:theme_genre_code]
+        @deck.playlists.draft.where(theme_genre_code: params[:theme_genre_code])
+      else
+        @deck.playlists.draft
+      end
     @deck_id = params[:deck_id].gsub('.json', '')
     @object_type = params[:type] || 'tvepisode'
     render json: { message: 'デッキが見つかりませんでした' }, status: 404 and return unless @deck
@@ -20,5 +26,5 @@ class External::DecksController < ApplicationController
       render json: { message: 'デッキが見つかりませんでした' }, status: 404
     end
   end
-  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 end
