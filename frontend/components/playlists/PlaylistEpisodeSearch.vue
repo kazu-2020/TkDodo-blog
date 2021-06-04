b62e15c042d670c8e37d9526d87cb18b5773b482
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2021-06-04 13:05:54 +0900

    e2 を検索しようとするとエラーになるため一旦除外

--- frontend/components/playlists/PlaylistEpisodeSearch.vue
+++ frontend/components/playlists/PlaylistEpisodeSearch.vue
@@ -248,7 +248,8 @@ export default Vue.extend({
 
       let searchUrl = `/episodes/search?${this.queryKey}=${this.editingKeywords}&offset=${this.searchOffset}&sort_type=${this.sortType}&ignore_range=${this.ignoreRange}&size=${pageSize}`
       if (this.filterService) {
-        searchUrl = searchUrl + '&service=g1,g2,e1,e2,e3'
+        // FIXME: e2 を加えると BadRequest になるため、一旦除外
+        searchUrl = searchUrl + '&service=g1,g2,e1,e3'
       }
       this.$axios
         .get(searchUrl)
