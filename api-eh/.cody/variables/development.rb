8eab3cc095a5c6d0c605dd631a250f67ca72f061
norikt <norikt@gmail.com>
2021-07-06 11:59:40 +0900

    更新漏れ

--- api-eh/.cody/variables/development.rb
+++ api-eh/.cody/variables/development.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-@target_linux_image = '359601428599.dkr.ecr.ap-northeast-1.amazonaws.com/aw-editorialhands-deployer:v2'
+@target_linux_image = '359601428599.dkr.ecr.ap-northeast-1.amazonaws.com/aw-editorialhands-deployer:v3'
 @env_values = {
   DOCKERHUB_USER: 'ssm:/aw-editorialhands/dev/dockerhub_user',
   DOCKERHUB_PASS: 'ssm:/aw-editorialhands/dev/dockerhub_pass',
