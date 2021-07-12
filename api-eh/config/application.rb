ca2a5266b5ba7006505195f3bcb3bd8c460a8d55
norikt <norikt@gmail.com>
2021-07-12 15:45:52 +0900

    Add localization

--- api-eh/config/application.rb
+++ api-eh/config/application.rb
@@ -19,7 +19,7 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Hoge
+module EditorialHandsAPI
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -29,7 +29,8 @@ module Hoge
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+
     # config.eager_load_paths << Rails.root.join("extras")
     config.eager_load_paths << Rails.root.join('app', 'services')
     config.eager_load_paths << Rails.root.join('app', 'uploaders')
@@ -37,6 +38,10 @@ module Hoge
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = %i[ja en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Permit cross origin
     config.middleware.insert_before 0, Rack::Cors do
