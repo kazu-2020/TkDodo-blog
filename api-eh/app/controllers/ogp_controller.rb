6d9d74f1c44cdf9332c30f18ef39e367d271a0ef
norikt <norikt@gmail.com>
2021-07-20 15:54:59 +0900

    Fix rubocop

--- api-eh/app/controllers/ogp_controller.rb
+++ api-eh/app/controllers/ogp_controller.rb
@@ -3,17 +3,12 @@
 # aw-pl-webから呼び出されるリッチリンク対応のためのエンドポイント
 class OgpController < ApplicationController
   def index
-    res = link_parse_params[:url] && Faraday.get(link_parse_params[:url])
+    res = ogp_params[:url] && Faraday.get(ogp_params[:url])
 
     if res&.success?
-      ogp = ogp(res.body)
-      html = Nokogiri::HTML.parse(res.body.to_s)
-
-      render json: { title: ogp&.title || ogp&.site_name || html.title.to_s,
-                     description: ogp&.description || html.css('//head/meta[name="description"]/@content'),
-                     image: ogp&.image&.url || 'https://placehold.jp/640x360.png' }
+      render json: make_json(res)
     else
-      render json: { message: "Error. url: #{link_parse_params[:url]}" }
+      render json: { message: "Error. url: #{ogp_params[:url]}" }
     end
   end
 
@@ -25,7 +20,17 @@ class OgpController < ApplicationController
     nil
   end
 
-  def link_parse_params
+  # @param [Faraday::Response] res
+  def make_json(res)
+    ogp = ogp(res.body)
+    html = Nokogiri::HTML.parse(res.body.to_s)
+
+    { title: ogp&.title || ogp&.site_name || html.title.to_s,
+      description: ogp&.description || html.css('//head/meta[name="description"]/@content'),
+      image: ogp&.image&.url || 'https://placehold.jp/640x360.png' }
+  end
+
+  def ogp_params
     params.permit(:url)
   end
 end
