141860e7543c7b44c94558f1c64b6bc7d31719de
norikt <norikt@gmail.com>
2021-09-01 17:48:40 +0900

    helperを修正

--- api-eh/app/helpers/embed/episodes_helper.rb
+++ api-eh/app/helpers/embed/episodes_helper.rb
@@ -2,18 +2,20 @@
 
 module Embed::EpisodesHelper
   def episode_image_src
-    eyecatch_image_url || keyvisual_url || series_eyecatch_image_url || '//via.placeholder.com/640x360'
+    episode_eyecatch_image_url || keyvisual_url || series_eyecatch_image_url || '//via.placeholder.com/640x360'
   end
 
+  private
+
   # TODO: episodeのeyecatchが無かった時にseriesのeyecatchを表示する
-  def eyecatch_image_url
+  def episode_eyecatch_image_url
     @episode_data.dig(:eyecatch, :medium, :url) ||
       @episode_data.dig(:eyecatch, :tver, :url) ||
       @episode_data.dig(:eyecatch, :main, :url)
   end
 
   def keyvisual_url
-    kv = @episode_data[:keyvisuals].first
+    kv = @episode_data[:keyvisuals]&.first
     return nil if kv.blank?
 
     kv.dig(:small, :url) || kv.dig(:large, :url) || kv.dig(:xlarge, :url)
