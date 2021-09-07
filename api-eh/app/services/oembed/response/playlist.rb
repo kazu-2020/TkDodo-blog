7d6de5f87130734253a41d61b4450d159cb13dfd
norikt <norikt@gmail.com>
2021-09-07 11:34:31 +0900

    Playlist用のoembedエンドポイントを更新

--- api-eh/app/services/oembed/response/playlist.rb
+++ api-eh/app/services/oembed/response/playlist.rb
@@ -5,8 +5,9 @@ class Oembed::Response::Playlist
 
   # rubocop:disable Metrics/MethodLength
   def response
+    playlist = Playlist.find(extract_playlist_id)
     src = "#{src_host}/embed/#{extract_playlist_url}"
-    height ||= 200
+    height ||= 210
     {
       version: '1.0',
       width: '100%',
@@ -15,10 +16,10 @@ class Oembed::Response::Playlist
       provider_name: 'NHK',
       provider_url: 'https://www.nhk.jp',
       url: url,
-      title: 'Series',
+      title: playlist.name,
       thumbnail_width: 640,
       thumbnail_height: 360,
-      thumbnail_url: 'https://placehold.jp/640x360.png',
+      thumbnail_url: playlist.logo_image_url,
       html: "<iframe width=\"100%\" height=\"#{height}\" src=\"#{src}\" style=\"border: 0;\"></iframe>"
     }
   end
@@ -29,5 +30,9 @@ class Oembed::Response::Playlist
   # @return [String] dev-embed.nr.nhk.jp用のURL pl/ 以降
   def extract_playlist_url
     url[%r{https?://dev-www-eh.nr.nhk.jp/p/(pl/eh-[A-Z0-9]{10})}, 1]
+  end
+
+  def extract_playlist_id
+    url[%r{https?://dev-www-eh.nr.nhk.jp/p/pl/eh-([A-Z0-9]{10})}, 1].to_i
   end
 end
