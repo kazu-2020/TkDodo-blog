74ff72e0caa2f76578f18c98a4dd64de77ca2bd7
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2021-09-13 13:27:02 +0900

    CodeBuild で仕様するベースイメージをアップデート

--- api-eh/.cody/variables/base.rb
+++ api-eh/.cody/variables/base.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-@target_linux_image = '359601428599.dkr.ecr.ap-northeast-1.amazonaws.com/aw-editorialhands-deployer:v5'
+@target_linux_image = '359601428599.dkr.ecr.ap-northeast-1.amazonaws.com/aw-editorialhands-deployer:v6'
 @env_values = {
   AWS_DEFAULT_REGION: 'ap-northeast-1',
   SLACK_NOTIFICATION_URL: 'https://hooks.slack.com/services/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',
