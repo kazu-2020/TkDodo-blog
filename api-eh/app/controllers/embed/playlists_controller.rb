e44876aa181ac908c01beb613a2849d976f298cf
norikt <norikt@gmail.com>
2021-09-16 09:56:11 +0900

    playlist embedのリンクを修正

--- api-eh/app/controllers/embed/playlists_controller.rb
+++ api-eh/app/controllers/embed/playlists_controller.rb
@@ -3,8 +3,8 @@
 class Embed::PlaylistsController < EmbedController
   def show # rubocop:disable Metrics/AbcSize
     playlist_id = params[:playlist_id].delete_prefix('eh-').to_i
-    @url = "https://dev-www-eh.nr.nhk.jp/p/pl/#{playlist_id}"
     @playlist = Playlist.find(playlist_id)
+    @url = "https://dev-www-eh.nr.nhk.jp/p/pl/#{@playlist.original_id}"
     @episode_data = @playlist.playlist_items.first.cached_data.symbolize_keys
     set_layout_pattern
     @height = embed_playlists_params[:height] || Oembed::Response::Playlist::DEFAULT_HEIGHT[@layout_pattern.to_sym]
