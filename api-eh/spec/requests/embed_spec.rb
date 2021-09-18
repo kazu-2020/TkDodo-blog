9ef71c4b9a5b2ff351043646175b1e61112d753f
norikt <norikt@gmail.com>
2021-09-18 16:22:28 +0900

    embedコンテンツ用のspecを追加

---
+++ api-eh/spec/requests/embed_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe EmbedController, type: :request do
+  describe 'Series' do
+    let(:series_id) { 'R71NJ4MV53' }
+
+    describe 'summary' do
+      let(:layout_pattern) { 'summary' }
+
+      it 'returns success response' do
+        VCR.use_cassette('dlab_api_series') do
+          get "/embed/ts/#{series_id}", params: { layout_pattern: layout_pattern }
+          expect(response.status).to eq 200
+        end
+      end
+    end
+
+    describe 'featuredItem' do
+      let(:layout_pattern) { 'featuredItem' }
+
+      it 'returns success response' do
+        VCR.use_cassette('dlab_api_episode_from_series') do
+          get "/embed/ts/#{series_id}", params: { layout_pattern: layout_pattern }
+          expect(response.status).to eq 200
+        end
+      end
+    end
+  end
+
+  describe 'Playlist' do
+    let(:playlist) { create(:playlist) }
+    let(:playlist_id) { format('%010<number>d', number: playlist.id) }
+
+    describe 'summary' do
+      let(:layout_pattern) { 'summary' }
+
+      it 'returns success response' do
+        get "/embed/pl/#{playlist_id}", params: { layout_pattern: layout_pattern }
+        expect(response.status).to eq 200
+      end
+    end
+
+    describe 'featuredItem' do
+      let(:layout_pattern) { 'featuredItem' }
+
+      it 'returns success response' do
+        get "/embed/pl/#{playlist_id}", params: { layout_pattern: layout_pattern }
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'Episode' do
+    it 'returns success response' do
+      series_id = 'R71NJ4MV53'
+      episode_id = 'B7ZJX92J2N'
+      VCR.use_cassette('embed_spec_episode') do
+        get "/embed/ts/#{series_id}/episode/te/#{episode_id}"
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'Howto' do
+    it 'returns success response' do
+      episode_id = '8X5KKJ1W5Y'
+      howto_id = '49'
+      VCR.use_cassette('embed_spec_howto') do
+        get "/embed/te/#{episode_id}/howto/#{howto_id}"
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'Event' do
+    it 'returns success response' do
+      episode_id = 'GV711R4WQV'
+      event_id = '94'
+      VCR.use_cassette('embed_spec_event') do
+        get "/embed/te/#{episode_id}/event/#{event_id}"
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe 'Faqpage' do
+    it 'returns success response' do
+      episode_id = '7NL2777YGX'
+      faqpage_id = '54'
+      VCR.use_cassette('embed_spec_faqpage') do
+        get "/embed/te/#{episode_id}/faqpage/#{faqpage_id}"
+        expect(response.status).to eq 200
+      end
+    end
+  end
+end
