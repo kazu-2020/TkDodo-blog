766a83305866e9c41171743360c4a3b08d507f1c
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2021-10-05 11:48:20 +0900

    テストを修正

--- frontend/spec/components/playlists/ColorPalette.spec.js
+++ frontend/spec/components/playlists/ColorPalette.spec.js
@@ -1,27 +1,45 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import { mount, RouterLinkStub } from '@vue/test-utils'
+import { mount, RouterLinkStub, createLocalVue } from '@vue/test-utils'
 import ColorPalette from '~/components/playlists/ColorPalette.vue'
 Vue.use(Vuetify)
 
 describe('components/playlists/ColorPalette.vue', () => {
+  const localVue = createLocalVue()
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
   it('Vue instance を作れている', () => {
     const wrapper = mount(ColorPalette, {
       stubs: {
         NuxtLink: RouterLinkStub,
       },
       propsData: { selectedPalette: '#005aff' },
+      localVue,
+      vuetify,
     })
     expect(wrapper.vm).toBeTruthy()
   })
 
   describe('Paletteを選択', () => {
+    const localVue = createLocalVue()
+    let vuetify
+
+    beforeEach(() => {
+      vuetify = new Vuetify()
+    })
+
     it('「update:selectedPalette」イベントが発行されること', () => {
       const wrapper = mount(ColorPalette, {
         stubs: {
           NuxtLink: RouterLinkStub,
         },
         propsData: { selectedPalette: '#005aff' },
+        localVue,
+        vuetify,
       })
       wrapper.find('button.palette').trigger('click')
 
