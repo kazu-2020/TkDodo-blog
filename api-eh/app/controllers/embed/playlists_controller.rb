426eeb04b8b3f5ad64a46508aaa91a7199071e9c
norikt <norikt@gmail.com>
2021-11-01 18:20:20 +0900

    github/view_componentを利用してリファクタリング

--- api-eh/app/controllers/embed/playlists_controller.rb
+++ api-eh/app/controllers/embed/playlists_controller.rb
@@ -2,23 +2,16 @@
 
 class Embed::PlaylistsController < EmbedController
   def show
-    set_layout_pattern
-    playlist_id = params[:playlist_id].delete_prefix('eh-').to_i
-    @view_data = Embed::BuildPlaylistViewData.new(playlist_id: playlist_id, layout_pattern: @layout_pattern).call
-    @height = embed_playlists_params[:height] ||
-              Oembed::Response::Playlist::DEFAULT_SIZE[@layout_pattern.to_sym][:height]
-
-    render 'embed/not_found', status: :not_found if @view_data.blank?
+    playlist_id = embed_params[:playlist_id].delete_prefix('eh-').to_i
+    @view_component = Embed::PlaylistViewComponentBuilder.new(playlist_id: playlist_id,
+                                                              height: embed_params[:height],
+                                                              layout_pattern: embed_params[:layout_pattern]).call
+    render 'embed/not_found', status: :not_found if @view_component.blank?
   end
 
   private
 
-  def embed_playlists_params
+  def embed_params
     params.permit(:playlist_id, :height, :layout_pattern)
-  end
-
-  def set_layout_pattern
-    is_exist_pattern = %w[summary largeImage featuredItem itemList].include?(embed_playlists_params[:layout_pattern])
-    @layout_pattern = is_exist_pattern ? embed_playlists_params[:layout_pattern] : 'summary'
   end
 end
---
+++ api-eh/app/services/embed/series_view_component_builder.rb
@@ -0,0 +1,67 @@
+class Embed::SeriesViewComponentBuilder
+  attr_reader :url, :height
+
+  def initialize(series_id:, height:, layout_pattern:)
+    @series_id = series_id
+    is_exist_pattern = %w[summary largeImage featuredItem itemList].include?(layout_pattern)
+    @layout_pattern = is_exist_pattern ? layout_pattern : 'summary'
+    @height = height || Oembed::Response::Series::DEFAULT_SIZE[@layout_pattern.to_sym][:height]
+  end
+
+  def call
+    case @layout_pattern
+    when 'summary'
+      build_summary_view_component
+    when 'featuredItem'
+      build_featured_item_view_component
+    when 'itemList'
+      build_item_list_view_component
+    end
+  rescue DlabApiClient::NotFound
+    nil
+  end
+
+  private
+
+  def build_summary_view_component
+    res = DlabApiClient.new(api_endpoint: 'https://api.nr.nhk.jp').series(type: 'tv', series_id: @series_id)
+    Embed::SummaryComponent.new(url: res[:url],
+                                name: res[:name],
+                                eyecatch_image_url: res.dig(:eyecatch, :medium, :url),
+                                description: res[:description],
+                                height: @height)
+  end
+
+  def build_featured_item_view_component
+    res = DlabApiClient.new(api_endpoint: 'https://api.nr.nhk.jp').episode_from_series(type: 'tv',
+                                                                                       series_id: @series_id,
+                                                                                       request_type: :l,
+                                                                                       query: { size: 1 })
+    episode_data = res[:result].first
+    series_data = episode_data[:partOfSeries]
+
+    Embed::FeaturedItemComponent.new(url: series_data[:url],
+                                     name: series_data[:name],
+                                     logo_image_url: series_data.dig(:logo, :medium, :url),
+                                     detailed_catch: series_data[:detailedCatch],
+                                     key_color: series_data.dig(:style, :primaryLight),
+                                     episode_data: episode_data,
+                                     height: @height)
+  end
+
+  def build_item_list_view_component
+    res = DlabApiClient.new(api_endpoint: 'https://api.nr.nhk.jp').episode_from_series(type: 'tv',
+                                                                                       series_id: @series_id,
+                                                                                       request_type: :l,
+                                                                                       query: { size: 3 })
+    episode_data = res[:result].first
+    series_data = episode_data[:partOfSeries]
+
+    Embed::ItemListComponent.new(url: series_data[:url],
+                                 name: series_data[:name],
+                                 hero_image_url: series_data.dig(:hero, :medium, :url),
+                                 key_color: series_data.dig(:style, :primaryLight),
+                                 episodes: res[:result],
+                                 height: @height)
+  end
+end
