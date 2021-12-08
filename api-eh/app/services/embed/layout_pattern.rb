ccd2aab073bf280e7f46efae015d652d72c1eb2e
norikt <norikt@gmail.com>
2021-12-08 18:03:26 +0900

    再生できないverを実装

--- api-eh/app/services/embed/layout_pattern.rb
+++ api-eh/app/services/embed/layout_pattern.rb
@@ -3,7 +3,7 @@ class Embed::LayoutPattern
     summary: { height: 210, width: '100%' },
     featuredItem: { height: 385, width: '240' },
     itemList: { height: 210, width: '240' },
-    largeImage: { height: 210, width: '100%' }
+    largeImage: { height: 360, width: '640' }
   }.freeze
 
   TYPES = %w[summary largeImage featuredItem itemList].freeze
