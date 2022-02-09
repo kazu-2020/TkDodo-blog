141c1a7f8445a33c2341abd6a2cd9bdc7648c22d
norikt <norikt@gmail.com>
2022-02-09 16:59:21 +0900

    Fix warning

--- api-eh/infra/dev/terraform/environments/modules/ecs.tf
+++ api-eh/infra/dev/terraform/environments/modules/ecs.tf
@@ -70,7 +70,7 @@ resource "aws_ecs_service" "ecs" {
 
 resource "aws_ecs_service" "ecs_sidekiq" {
   name            = "${local.env_resource_prefix}-service-sidekiq"
-  cluster         = "${aws_ecs_cluster.ecs_cluster.id}"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = "${local.env_resource_prefix}:1"
   # サービス作成時は必要数0にしておいて、デプロイのときに初めて必要数を設定する
   desired_count                      = 0
@@ -81,12 +81,12 @@ resource "aws_ecs_service" "ecs_sidekiq" {
 
   network_configuration {
     subnets = [
-      "${lookup(var.subnet_private_a, "${terraform.workspace}")}",
-      "${lookup(var.subnet_private_c, "${terraform.workspace}")}",
+      lookup(var.subnet_private_a, terraform.workspace),
+      lookup(var.subnet_private_c, terraform.workspace),
     ]
 
     security_groups = [
-      "${lookup(var.app_security_group, "${terraform.workspace}")}",
+      lookup(var.app_security_group, terraform.workspace),
     ]
   }
 
@@ -99,9 +99,9 @@ resource "aws_ecs_service" "ecs_sidekiq" {
   # deployやautoscaleで動的に変化する値を差分だしたくないので無視する
   lifecycle {
     ignore_changes = [
-      "desired_count",
-      "task_definition",
-      "load_balancer",
+      desired_count,
+      task_definition,
+      load_balancer,
     ]
   }
 }
