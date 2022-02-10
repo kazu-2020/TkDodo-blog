c0cbd6dbd9e647bcc8e68da73f4c7a76542d09bc
norikt <norikt@gmail.com>
2022-02-10 11:56:10 +0900

    キューの設定を修正

--- api-eh/app/jobs/null_job.rb
+++ api-eh/app/jobs/null_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class NullJob < SidekiqBaseJob
-  sidekiq_options queue: 'low'
+  sidekiq_options queue: 'p1'
 
   def perform
     Sidekiq.❨╯°□°❩╯︵┻━┻ # rubocop:disable Naming/AsciiIdentifiers
