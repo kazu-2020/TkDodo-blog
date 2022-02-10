c0c939b20454011f9a602afe5c021f16605d8d7c
norikt <norikt@gmail.com>
2022-02-10 14:05:36 +0900

    redisのパラメータをsidekiq推奨のものに変更

--- api-eh/infra/dev/terraform/environments/modules/elasticache.tf
+++ api-eh/infra/dev/terraform/environments/modules/elasticache.tf
@@ -11,7 +11,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = lookup(var.elasticache, "${terraform.workspace}.node_type")
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
+  parameter_group_name = "${local.env_resource_prefix}-redis-params"
   engine_version       = "6.x"
   port                 = 6379
   subnet_group_name    = "${local.env_resource_prefix}-redis-subnet"
@@ -23,5 +23,16 @@ resource "aws_elasticache_cluster" "redis" {
     Name        = "${var.name}-${lookup(var.redis_cluster_id_key, "${terraform.workspace}.key")}"
     Stack       = var.name
     Environment = terraform.workspace
+  }
+}
+
+resource "aws_elasticache_parameter_group" "default" {
+  name   = "${local.env_resource_prefix}-redis-params"
+  family = "redis6.x"
+
+  parameter {
+    # sidekiq推奨のパラメータ https://github.com/mperham/sidekiq/wiki/Using-Redis#memory
+    name  = "maxmemory-policy"
+    value = "noeviction"
   }
 }
