6b39ef9219071b5b4408f9f4c6db3a341d81c3be
norikt <norikt@gmail.com>
2022-03-11 11:41:52 +0900

    Fix deploy.sh

--- api-eh/infra/stg/containers/ecs/bin/deploy.sh
+++ api-eh/infra/stg/containers/ecs/bin/deploy.sh
@@ -16,15 +16,16 @@ if [ -n "$ENV" -a "$ENV" = "staging" ]; then
   export TOTAL_MEMORY=2048 # 1024 MB
   export RAILS_MEMORY=2048
   export SIDEKIQ_MEMORY=2048
-  export ALB_TARGET_ARN=dummy
-  export DESIRED_COUNT=1
+  export ALB_TARGET_ARN=arn:aws:elasticloadbalancing:ap-northeast-1:312328096018:targetgroup/tomigaya-stg-api-eh-tg/2e9bcbb2bed38a67
+  export DESIRED_COUNT=2
   export DESIRED_COUNT_SIDEKIQ=1
+  export ENV_KEY=stg
 
   # コンテナに渡す環境変数(circleciで設定)
-  cat < ./api-eh/infra/stg/containers/ecs/common.env > ./api-eh/infra/stg/containers/ecs/${ENV}.env.gen
-  cat < ./api-eh/infra/stg/containers/ecs/${ENV}.env >> ./api-eh/infra/stg/containers/ecs/${ENV}.env.gen
-  cat >> ./api-eh/infra/stg/containers/ecs/${ENV}.env.gen  <<FIN
-RAILS_ENV=staging
+  cat < ./api-eh/infra/${ENV_KEY}/containers/ecs/common.env > ./api-eh/infra/${ENV_KEY}/containers/ecs/${ENV}.env.gen
+  cat < ./api-eh/infra/${ENV_KEY}/containers/ecs/${ENV}.env >> ./api-eh/infra/${ENV_KEY}/containers/ecs/${ENV}.env.gen
+  cat >> ./api-eh/infra/${ENV_KEY}/containers/ecs/${ENV}.env.gen  <<FIN
+RAILS_ENV=${ENV}
 RAILS_MASTER_KEY=${RAILS_MASTER_KEY_PRODUCTION}
 FIN
 fi
@@ -34,10 +35,10 @@ fi
 up_web() {
   echo start up web
   # タスク定義生成
-  ruby ./api-eh/infra/stg/containers/ecs/gen_task_def.rb \
-    --env_file ./api-eh/infra/stg/containers/ecs/${ENV}.env.gen \
-    --secrets-file ./api-eh/infra/stg/containers/ecs/secrets.yml \
-    --task-definition-template ./api-eh/infra/stg/containers/ecs/task-definition-template.json | jq '.' > task-definitions.json
+  ruby ./api-eh/infra/${ENV}/containers/ecs/gen_task_def.rb \
+    --env_file ./api-eh/infra/${ENV}/containers/ecs/${ENV}.env.gen \
+    --secrets-file ./api-eh/infra/${ENV}/containers/ecs/secrets.yml \
+    --task-definition-template ./api-eh/infra/${ENV}/containers/ecs/task-definition-template.json | jq '.' > task-definitions.json
   cat task-definitions.json
 
   # タスク定義登録
@@ -45,12 +46,12 @@ up_web() {
     --cli-input-json file://task-definitions.json
 
   # 最新のタスク定義確認
-  local latest_task_definition=$(latest_task_definition ${APP_PREFIX}${ENV}-${CLUSTER_APP_NAME})
+  local latest_task_definition=$(latest_task_definition ${APP_PREFIX}${ENV_KEY}-${CLUSTER_APP_NAME})
 
   # 最新のタスク定義でサービス更新
   aws ecs update-service \
-    --cluster ${APP_PREFIX}${ENV}-${CLUSTER_APP_NAME}-ecs-cluster \
-    --service ${APP_PREFIX}${ENV}-${CLUSTER_APP_NAME}-service \
+    --cluster ${APP_PREFIX}${ENV_KEY}-${CLUSTER_APP_NAME}-ecs-cluster \
+    --service ${APP_PREFIX}${ENV_KEY}-${CLUSTER_APP_NAME}-service \
     --enable-execute-command \
     --deployment-configuration "deploymentCircuitBreaker={enable=true,rollback=true}" \
     --task-definition ${latest_task_definition} \
@@ -62,10 +63,10 @@ up_web() {
   # sidekiq
   echo sidekiq serivce update start
   # sidekiqのタスク定義生成
-  ruby ./api-eh/infra/stg/containers/ecs/gen_task_def.rb \
-    --env_file ./api-eh/infra/stg/containers/ecs/${ENV}.env.gen \
-    --secrets-file ./api-eh/infra/stg/containers/ecs/secrets.yml \
-    --task-definition-template ./api-eh/infra/stg/containers/ecs/task-definition-template-sidekiq.json | jq '.' > task-definitions-sidekiq.json
+  ruby ./api-eh/infra/${ENV}/containers/ecs/gen_task_def.rb \
+    --env_file ./api-eh/infra/${ENV}/containers/ecs/${ENV}.env.gen \
+    --secrets-file ./api-eh/infra/${ENV}/containers/ecs/secrets.yml \
+    --task-definition-template ./api-eh/infra/${ENV}/containers/ecs/task-definition-template-sidekiq.json | jq '.' > task-definitions-sidekiq.json
   cat task-definitions-sidekiq.json
 
   # sidekiqタスク定義登録
@@ -73,12 +74,12 @@ up_web() {
     --cli-input-json file://task-definitions-sidekiq.json
 
   # sidekiqの最新のタスク定義確認
-  local latest_task_definition_sidekiq=$(latest_task_definition ${APP_PREFIX}${ENV}-${CLUSTER_APP_NAME})
+  local latest_task_definition_sidekiq=$(latest_task_definition ${APP_PREFIX}${ENV_KEY}-${CLUSTER_APP_NAME})
 
   # 最新のタスク定義でsidekiqのサービス更新
   aws ecs update-service \
-    --cluster ${APP_PREFIX}${ENV}-${CLUSTER_APP_NAME}-ecs-cluster \
-    --service ${APP_PREFIX}${ENV}-${CLUSTER_APP_NAME}-service-sidekiq \
+    --cluster ${APP_PREFIX}${ENV_KEY}-${CLUSTER_APP_NAME}-ecs-cluster \
+    --service ${APP_PREFIX}${ENV_KEY}-${CLUSTER_APP_NAME}-service-sidekiq \
     --enable-execute-command \
     --deployment-configuration "deploymentCircuitBreaker={enable=true,rollback=true}" \
     --task-definition ${latest_task_definition_sidekiq} \
