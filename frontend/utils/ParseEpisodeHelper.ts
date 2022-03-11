9850e024cfd7d2ac03ec0c456d00d89d6b555edc
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2022-03-11 11:00:13 +0900

    ビデオ判定周りをリファクタ

--- frontend/utils/ParseEpisodeHelper.ts
+++ frontend/utils/ParseEpisodeHelper.ts
@@ -1,13 +1,30 @@
 import { EpisodeData } from '@/types/episode_data'
 
 export default class {
+  static isOkushibuVideo(video: any) {
+    return (
+      video.detailedContentStatus?.environmentId === 'okushibu' &&
+      video.detailedContentStatus?.contentStatus === 'ready'
+    )
+  }
+
   static hasVideo(episode: EpisodeData) {
     const videos = episode?.videos || []
-    const okushibuVideo = videos.find(
-      (video: any) =>
-        video.detailedContentStatus?.environmentId === 'okushibu' &&
-        video.detailedContentStatus?.contentStatus === 'ready'
+    const okushibuVideo = videos.find((video: any) =>
+      this.isOkushibuVideo(video)
     )
     return !!okushibuVideo
+  }
+
+  static videoUrl(episode: EpisodeData) {
+    const videos = episode?.videos || []
+    const okushibuVideo: any = videos.find((video: any) =>
+      this.isOkushibuVideo(video)
+    )
+
+    const hlsVideo = (okushibuVideo?.detailedContent || []).find(
+      (v: any) => v.name === 'hls_fmp4'
+    )
+    return hlsVideo?.contentUrl
   }
 }
