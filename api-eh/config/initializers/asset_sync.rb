1055f01ded8ef4afb4aefb6b558e70ecf61f8246
norikt <norikt@gmail.com>
2022-03-15 19:05:03 +0900

    assetsのバケット名を修正

--- api-eh/config/initializers/asset_sync.rb
+++ api-eh/config/initializers/asset_sync.rb
@@ -20,9 +20,9 @@ if defined?(AssetSync)
     config.fog_directory = if Rails.env.dev?
                              'tomigaya-dev-api-eh-assets'
                            elsif Rails.env.staging?
-                             'tomigaya-stg-api-eh-assets'
+                             'tomigaya-stg-editorialhands-assets'
                            elsif Rails.env.production?
-                             'tomigaya-prd-api-eh-assets'
+                             'tomigaya-prd-editorialhands-assets'
                            elsif Rails.env.test? || Rails.env.development?
                              'dummy'
                            else
