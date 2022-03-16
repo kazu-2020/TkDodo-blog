7667b5f2997be44a7e5ce6b84aeb8498ab7a34cf
norikt <norikt@gmail.com>
2022-03-16 11:02:53 +0900

    hosting bucketにaws_s3_bucket_public_access_blockを追加

--- api-eh/infra/stg/terraform/global/modules/s3.tf
+++ api-eh/infra/stg/terraform/global/modules/s3.tf
@@ -81,3 +81,12 @@ resource "aws_s3_bucket" "hosting_bucket" {
 }
 EOF
 }
+
+resource "aws_s3_bucket_public_access_block" "hosting_bucket" {
+  bucket = aws_s3_bucket.hosting_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
