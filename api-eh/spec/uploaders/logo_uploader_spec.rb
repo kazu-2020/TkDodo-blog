f0d327d4dfeb7f48f069f4db5ca8ad2f5561ef71
norikt <norikt@gmail.com>
2022-03-29 19:05:35 +0900

    Fix spec

--- api-eh/spec/uploaders/logo_uploader_spec.rb
+++ api-eh/spec/uploaders/logo_uploader_spec.rb
@@ -9,12 +9,13 @@ RSpec.describe LogoUploader, type: :model do
 
   describe :strip do
     it do
+      playlist.reload
       expect(derivatives[:default]).to be_kind_of(Shrine::UploadedFile)
       expect(derivatives[:small]).to be_kind_of(Shrine::UploadedFile)
       expect(derivatives[:medium]).to be_kind_of(Shrine::UploadedFile)
 
       # exifが消えてるか確認
-      path = File.expand_path("public/uploads/test/#{image.id}")
+      path = File.expand_path("public/uploads/test/private/#{image.id}")
       image = MiniMagick::Image.open(path)
       expect(image.exif).to be_blank
     end
