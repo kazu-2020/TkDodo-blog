d713655b408e442590aa119179963463513bcbd8
norikt <norikt@gmail.com>
2022-03-29 16:23:53 +0900

    公開バケットを追加
    ※各バケットともにポリシーにIAMロールの設定が必要

--- api-eh/infra/prd/terraform/global/modules/s3.tf
+++ api-eh/infra/prd/terraform/global/modules/s3.tf
@@ -1,3 +1,5 @@
+# ----------------------------------------
+# 画像の非公開バケット（全部入り）
 resource "aws_cloudfront_origin_access_identity" "resources_bucket" {
   comment = "${local.global_resource_prefix}-resources-bucket"
 }
@@ -45,6 +47,53 @@ resource "aws_s3_bucket_public_access_block" "resources_bucket" {
   restrict_public_buckets = true
 }
 
+# ----------------------------------------
+# 画像の公開バケット
+resource "aws_s3_bucket" "images_bucket" {
+  bucket = "${local.global_resource_prefix}-images"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+  # FIXME: 許可するIAMユーザーを設定する
+  #  policy = <<EOF
+  #{
+  #    "Version": "2012-10-17",
+  #    "Statement": [
+  #        {
+  #            "Sid": "VisualEditor0",
+  #            "Effect": "Allow",
+  #            "Action": "s3:GetObject",
+  #            "Principal": {
+  #                "AWS": "arn:aws:iam::AccountA-ID:user/Dave"
+  #            },
+  #            "Resource": "arn:aws:s3:::${local.global_resource_prefix}-images/*"
+  #        }
+  #    ]
+  #}
+  #EOF
+}
+
+resource "aws_s3_bucket_public_access_block" "images_bucket" {
+  bucket = aws_s3_bucket.images_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# ----------------------------------------
+# frontend appのホスティング用バケット
 resource "aws_cloudfront_origin_access_identity" "hosting_bucket" {
   comment = "${local.global_resource_prefix}-hosting-bucket"
 }
