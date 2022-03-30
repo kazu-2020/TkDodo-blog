475d71a16573f45232b82371b207454fb1a27f47
norikt <norikt@gmail.com>
2022-03-30 14:41:06 +0900

    バケットの変更に合わせてlocal環境の画像のパスを修正

--- frontend/env.development.js
+++ frontend/env.development.js
@@ -1,6 +1,6 @@
 module.exports = {
   apiBaseUrl: 'http://localhost:8888',
-  imageBaseUrl: 'http://localhost:8888/uploads/',
+  imageBaseUrl: 'http://localhost:8888/uploads/private/',
   AUTH0_DOMAIN: 'dev-9ynu55y2.us.auth0.com',
   AUTH0_CLIENT_ID: 'PT90Tj6z7yuxMmPdFKO8b0sfdLzmX4qI',
   AUTH0_AUDIENCE: 'https://dev-9ynu55y2.us.auth0.com/api/v2/',
