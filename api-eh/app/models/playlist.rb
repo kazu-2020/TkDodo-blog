def5ad1b2a5472217016a90500ae05c3916db0c6
norikt <norikt@gmail.com>
2022-04-01 16:33:10 +0900

    available_articleをtrueにする条件を変更
    https://github.com/d7lab/aw-editorialhands/issues/1000

--- api-eh/app/models/playlist.rb
+++ api-eh/app/models/playlist.rb
@@ -182,7 +182,7 @@ class Playlist < ApplicationRecord # rubocop:disable Metrics/ClassLength
   end
 
   def set_available_article
-    self.available_article = has_article?
+    self.available_article = has_article? && active_article?
   end
 
   # @param [String] type blockのtypeの文字列
