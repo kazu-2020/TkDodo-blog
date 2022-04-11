47c7e0e395abbe222b1226975dde21d128bda40a
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-04-11 14:23:27 +0900

    fix：factories/decksのインデントを修正
    rubocopを通過させておらずCIがコケていたため、修正

--- api-eh/spec/factories/decks.rb
+++ api-eh/spec/factories/decks.rb
@@ -4,12 +4,12 @@ FactoryBot.define do
   factory :deck do
     sequence(:id) { |n| n }
     sequence(:name) { |n| "deck #{n}" }
-    description { "description for the sample " }
+    description { 'description for the sample ' }
     interfix { 'sample' }
     area { 130 }
-    admin_memo { "test memo" }
+    admin_memo { 'test memo' }
     deck_uid { SecureRandom.uuid }
-    created_at{ "1900-01-01T00:00:00+09:00" }
-    updated_at{ "1900-01-01T00:00:00+09:00" }
+    created_at { '1900-01-01T00:00:00+09:00' }
+    updated_at { '1900-01-01T00:00:00+09:00' }
   end
 end
