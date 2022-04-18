9199174404ac89cb8d792f43fe8cfb216b8dd591
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-04-18 09:40:58 +0900

    test
    validatesのname,prefixはcontrollerクラスのテストで挙動を担保しているため未実装
    rebuild_playlists_toメソッドはcontrollerクラスのテストで挙動を担保しているため未実装
    total_timeメソッドはplaylistsテーブルに該当するカラムが存在しないため未実装

--- api-eh/spec/models/deck_spec.rb
+++ api-eh/spec/models/deck_spec.rb
@@ -5,25 +5,64 @@ require 'rails_helper'
 describe Deck, type: :model do
   describe 'before_validation' do
     context 'deck_uidがblankの場合' do
-      it 'deck_uidにランダムな値が代入されること'
+      it 'deck_uidに値が代入されること' do
+        deck_without = build(:deck, deck_uid: nil)
+        deck_without.save
+        expect(deck_without.deck_uid).not_to eq nil
+      end
     end
+
+    context 'deck_uidがblankではない場合' do
+      it 'deck_uidが「test」となること' do
+        deck_without = build(:deck, deck_uid: 'test')
+        expect(deck_without.deck_uid).to eq 'test'
+      end
+    end
+
     context 'アイテムモードが未定義の場合' do
-      it 'アイテムモードが「tv」となること'
+      it 'アイテムモードが「tv」となること' do
+        deck_undefined = Deck.new
+        expect(deck_undefined.mode_of_item).to eq 'tv'
+      end
     end
+
+    context 'アイテムモードが定義されている場合' do
+      it 'アイテムモードが「test」となること' do
+        deck_undefined = Deck.new(mode_of_item: 'test')
+        expect(deck_undefined.mode_of_item).to eq 'test'
+      end
+    end
+
     context 'アイテムタイプが未定義の場合' do
-      it 'アイテムタイプが「TVEpisode」となること'
+      it 'アイテムタイプが「TVEpisode」となること' do
+        deck_undefined = Deck.new
+        expect(deck_undefined.type_of_item).to eq 'TVEpisode'
+      end
+    end
+
+    context 'アイテムタイプが定義されている場合' do
+      it 'アイテムタイプが「test」となること' do
+        deck_undefined = Deck.new(type_of_item: 'test')
+        expect(deck_undefined.type_of_item).to eq 'test'
+      end
     end
   end
 
   describe 'before_save' do
-    it 'deck_idが取得されないこと'
+    it 'deck_idがnilであること' do
+      deck = build(:deck)
+      expect(deck.deck_id).to be_nil
+    end
   end
 
-  context 'after_create' do
-    it 'deck_idが取得されること'
-  end
-
-  context 'total_time' do
-    it 'do nothing'
+  describe 'after_create' do
+    it 'deck_idが取得されること' do
+      deck = build(:deck)
+      expect do
+        deck.save
+      end.to change {
+        deck.deck_id.present?
+      }.from(false).to(true)
+    end
   end
 end
