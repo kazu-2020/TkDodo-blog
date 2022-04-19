2b22b55d8e2549ea3f4c2c4e743ee760337aafc3
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-04-19 15:03:33 +0900

    fix：レビュー箇所の修正

--- api-eh/spec/models/deck_spec.rb
+++ api-eh/spec/models/deck_spec.rb
@@ -4,46 +4,23 @@ require 'rails_helper'
 
 describe Deck, type: :model do
   describe 'before_validation' do
-    context 'deck_uidがblankの場合' do
-      it 'deck_uidに値が代入されること' do
-        deck_without = build(:deck, deck_uid: nil)
-        deck_without.save
-        expect(deck_without.deck_uid).not_to eq nil
-      end
-    end
-
-    context 'deck_uidがblankではない場合' do
-      it 'deck_uidが「test」となること' do
-        deck_without = build(:deck, deck_uid: 'test')
-        expect(deck_without.deck_uid).to eq 'test'
-      end
-    end
-
-    context 'アイテムモードが未定義の場合' do
-      it 'アイテムモードが「tv」となること' do
-        deck_undefined = Deck.new
+    context 'デフォルト値が定義されていない場合' do
+      it 'deck_uidにuuid、mode_of_itemに「tv」、item_of_typeに「TVEpisode」が設定されること' do
+        deck_undefined = build(:deck, deck_uid: nil)
+        deck_undefined.save
+        expect(deck_undefined.deck_uid).not_to eq nil
         expect(deck_undefined.mode_of_item).to eq 'tv'
-      end
-    end
-
-    context 'アイテムモードが定義されている場合' do
-      it 'アイテムモードが「test」となること' do
-        deck_undefined = Deck.new(mode_of_item: 'test')
-        expect(deck_undefined.mode_of_item).to eq 'test'
-      end
-    end
-
-    context 'アイテムタイプが未定義の場合' do
-      it 'アイテムタイプが「TVEpisode」となること' do
-        deck_undefined = Deck.new
         expect(deck_undefined.type_of_item).to eq 'TVEpisode'
       end
     end
 
-    context 'アイテムタイプが定義されている場合' do
-      it 'アイテムタイプが「test」となること' do
-        deck_undefined = Deck.new(type_of_item: 'test')
-        expect(deck_undefined.type_of_item).to eq 'test'
+    context 'デフォルト値が定義されている場合' do
+      it 'deck_uidにuuid、mode_of_itemに「tv」、item_of_typeに「TVEpisode」が設定されないこと' do
+        deck_undefined = build(:deck, deck_uid: 'test', mode_of_item: 'test_item', type_of_item: 'test_type')
+        deck_undefined.save
+        expect(deck_undefined.deck_uid).to eq 'test'
+        expect(deck_undefined.mode_of_item).to eq 'test_item'
+        expect(deck_undefined.type_of_item).to eq 'test_type'
       end
     end
   end
