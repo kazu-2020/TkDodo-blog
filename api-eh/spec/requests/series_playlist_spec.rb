69201f29d3b126f328c484b030fb6a9d743790dd
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-04-21 09:11:14 +0900

    fix：シリーズIDを修正

--- api-eh/spec/requests/series_playlist_spec.rb
+++ api-eh/spec/requests/series_playlist_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe SeriesPlaylist, type: :request do
-  let(:series_id) { 'Y3XNWR54KL' }
+  let(:series_id) { '5NVVN1G5PJ' }
   let(:string_id) { "ts-#{series_id}" }
   let!(:series_playlist) { create(:series_playlist, string_id: string_id, series_id: series_id) }
 
@@ -17,7 +17,7 @@ describe SeriesPlaylist, type: :request do
   describe '#GET search' do
     it '正常にレスポンスが返ってくること' do
       VCR.use_cassette('requests/series_playlists/search_path') do
-        get '/series_playlists/search'
+        get search_series_playlists_path
         expect(response.status).to eq 200
       end
     end
