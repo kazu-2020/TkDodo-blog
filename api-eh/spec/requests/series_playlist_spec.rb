9624d7c12d3146ac79b099b539a81e7c94c1ffad
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-04-26 09:53:58 +0900

    test：データが取得されないケースを追加

--- api-eh/spec/requests/series_playlist_spec.rb
+++ api-eh/spec/requests/series_playlist_spec.rb
@@ -9,17 +9,6 @@ describe SeriesPlaylist, type: :request do
   let!(:has_episodes) { create(:series_playlist, string_id: 'ts-1V1PJ9L5JN', series_id: '1V1PJ9L5JN') }
 
   describe '#GET episodes' do
-    context 'idに紐づくデータが存在しない場合' do
-      it 'データは取得されないが、レスポンスは200として処理されること' do
-        VCR.use_cassette('requests/series_playlists/no_episodes') do
-          get "/series_playlists/#{has_not_episodes.id}/episodes"
-          expect(response.status).to eq 200
-          json = JSON.parse(response.body)
-          expect(json['episodes'].count).to eq 0
-        end
-      end
-    end
-
     context 'idに紐づくデータが存在する場合' do
       it 'データが取得できること' do
         VCR.use_cassette('requests/series_playlists/episodes') do
@@ -30,13 +19,39 @@ describe SeriesPlaylist, type: :request do
         end
       end
     end
+
+    context 'idに紐づくデータが存在しない場合' do
+      it 'データは取得されないが、レスポンスは200として処理されること' do
+        VCR.use_cassette('requests/series_playlists/no_episodes') do
+          get "/series_playlists/#{has_not_episodes.id}/episodes"
+          expect(response.status).to eq 200
+          json = JSON.parse(response.body)
+          expect(json['episodes'].count).to eq 0
+        end
+      end
+    end
   end
 
   describe '#GET search' do
-    it '正常にレスポンスが返ってくること' do
-      VCR.use_cassette('requests/series_playlists/search') do
-        get search_series_playlists_path
-        expect(response.status).to eq 200
+    context '検索ワードに該当するデータがある場合' do
+      it 'データが取得できること' do
+        VCR.use_cassette('requests/series_playlists/search_return_results') do
+          get search_series_playlists_path
+          expect(response.status).to eq 200
+          json = JSON.parse(response.body)
+          expect(json['count']).not_to eq 0
+        end
+      end
+    end
+
+    context '検索ワードに該当するデータが無い場合' do
+      it 'データは取得されないが、レスポンスは200として処理されること' do
+        VCR.use_cassette('requests/series_playlists/search_return_no_results') do
+          get search_series_playlists_path, params: { word: 'expected no search results' }
+          expect(response.status).to eq 200
+          json = JSON.parse(response.body)
+          expect(json['count']).to eq 0
+        end
       end
     end
   end
