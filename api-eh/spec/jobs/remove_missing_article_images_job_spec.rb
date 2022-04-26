a837f9ad5c09d30eea96d0a96ffb505ec7f68acb
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-04-26 18:29:49 +0900

    fix：レビュー指摘箇所の修正

--- api-eh/spec/jobs/remove_missing_article_images_job_spec.rb
+++ api-eh/spec/jobs/remove_missing_article_images_job_spec.rb
@@ -3,11 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe RemoveMissingArticleImagesJob, type: :model do
-  it '古い画像だけ削除されること' do
-    create(:article_image, :with_image, created_at: 90.days.ago, playlist_id: nil)
-    create(:article_image, :with_image, created_at: Time.now, playlist_id: nil)
+  let(:playlist) { create(:playlist) }
+
+  before do
+    create(:article_image, :with_image, created_at: 90.days.ago, playlist_id: playlist.id)  # playlist_article_image
+    create(:article_image, :with_image, created_at: 90.days.ago, playlist_id: nil)          # old_missing_image
+    create(:article_image, :with_image, created_at: Time.now, playlist_id: nil)             # missing_image
+  end
+
+  it 'プレイリストと紐づかない古い画像だけ削除されること' do
     expect {
       described_class.perform_async
-    }.to change(ArticleImage, :count).from(2).to(1)
+    }.to change(ArticleImage, :count).from(3).to(2)
   end
 end
--- api-eh/spec/requests/richlink_controller_spec.rb
+++ api-eh/spec/requests/richlink_controller_spec.rb
@@ -6,7 +6,7 @@ describe RichlinkController, type: :request do
   describe 'GET /richlink' do
     let!(:playlist) { create(:playlist) }
 
-    context 'NHK公式ホームページからアクセスした場合' do
+    context 'paramsに新標準のURLを設定した場合' do
       it 'レスポンスが正常に返ってくること' do
         get richlink_url, params: { url: 'https://www.nhk.or.jp/' }
 
@@ -15,7 +15,7 @@ describe RichlinkController, type: :request do
       end
     end
 
-    context 'dev環境からアクセスした場合' do
+    context 'paramsにプレイリスト新標準プロトタイプのプレイリストURLを設定した場合' do
       it 'レスポンスが正常に返ってくること' do
         playlist_id = format('%010<number>d', number: playlist.id)
         get richlink_url, params: { url: "https://dev-www-eh.nr.nhk.jp/p/pl/recommend-tep-#{playlist_id}" }
