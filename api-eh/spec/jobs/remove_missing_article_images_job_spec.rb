91348dfd3a180bc096cbae4f40fcaf9fc2d812c3
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-04-26 09:29:25 +0900

    fix：レビュー指摘箇所の修正
    LetSetup
    RepeatedDescription
    を有効化

--- api-eh/spec/jobs/remove_missing_article_images_job_spec.rb
+++ api-eh/spec/jobs/remove_missing_article_images_job_spec.rb
@@ -3,16 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe RemoveMissingArticleImagesJob, type: :model do
-  let(:playlist) { create(:playlist) }
-  let!(:playlist_article_image) do
-    create(:article_image, :with_image, created_at: 90.days.ago, playlist_id: playlist.id)
-  end
-  let!(:old_missing_image) { create(:article_image, :with_image, created_at: 90.days.ago, playlist_id: nil) }
-  let!(:missing_image) { create(:article_image, :with_image, created_at: Time.now, playlist_id: nil) }
-
   it '古い画像だけ削除されること' do
+    create(:article_image, :with_image, created_at: 90.days.ago, playlist_id: nil)
+    create(:article_image, :with_image, created_at: Time.now, playlist_id: nil)
     expect {
       described_class.perform_async
-    }.to change(ArticleImage, :count).from(3).to(2)
+    }.to change(ArticleImage, :count).from(2).to(1)
   end
 end
--- api-eh/spec/requests/richlink_controller_spec.rb
+++ api-eh/spec/requests/richlink_controller_spec.rb
@@ -6,19 +6,23 @@ describe RichlinkController, type: :request do
   describe 'GET /richlink' do
     let!(:playlist) { create(:playlist) }
 
-    it 'returns success response' do
-      get richlink_url, params: { url: 'https://www.nhk.or.jp/' }
+    context 'NHK公式ホームページからアクセスした場合' do
+      it 'レスポンスが正常に返ってくること' do
+        get richlink_url, params: { url: 'https://www.nhk.or.jp/' }
 
-      expect(response.status).to eq 200
-      expect(response.body).not_to include 'Error'
+        expect(response.status).to eq 200
+        expect(response.body).not_to include 'Error'
+      end
     end
 
-    it 'returns success response' do
-      playlist_id = format('%010<number>d', number: playlist.id)
-      get richlink_url, params: { url: "https://dev-www-eh.nr.nhk.jp/p/pl/recommend-tep-#{playlist_id}" }
+    context 'dev環境からアクセスした場合' do
+      it 'レスポンスが正常に返ってくること' do
+        playlist_id = format('%010<number>d', number: playlist.id)
+        get richlink_url, params: { url: "https://dev-www-eh.nr.nhk.jp/p/pl/recommend-tep-#{playlist_id}" }
 
-      expect(response.status).to eq 200
-      expect(response.body).not_to include 'Error'
+        expect(response.status).to eq 200
+        expect(response.body).not_to include 'Error'
+      end
     end
   end
 end
