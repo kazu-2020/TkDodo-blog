fc02b35dea5dbd56d487bc94ccf876c07c95f6ae
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-04-27 09:51:27 +0900

    fix

--- api-eh/spec/requests/playlists_spec.rb
+++ api-eh/spec/requests/playlists_spec.rb
@@ -94,6 +94,7 @@ describe PlaylistsController, type: :request do
 
   describe 'POST #create' do
     let(:params) { { playlist: { name: 'cool name' } } }
+    before { create(:deck) }
 
     it 'creates new playlist' do
       expect do
