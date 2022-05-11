ecac5f96800eb5fc6c1eab019fd9a0abcf523cb4
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-05-11 10:44:40 +0900

    fix：検索のテストに検索ワードを追加

--- api-eh/spec/requests/series_playlist_spec.rb
+++ api-eh/spec/requests/series_playlist_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 describe SeriesPlaylist, type: :request do
-  # 2022/04/25時点で、IDが１の時エピソードが存在しない
+  # 2022/04/25時点でエピソードが存在しないseries_idで生成
   let!(:has_not_episodes) { create(:series_playlist, string_id: 'ts-5NVVN1G5PJ', series_id: '5NVVN1G5PJ') }
-  # 2022/04/25時点で、IDが2の時エピソードが存在する
+  # 2022/04/25時点でエピソードが存在するseries_idで生成
   let!(:has_episodes) { create(:series_playlist, string_id: 'ts-1V1PJ9L5JN', series_id: '1V1PJ9L5JN') }
 
   describe '#GET episodes' do
@@ -38,11 +38,11 @@ describe SeriesPlaylist, type: :request do
     context '検索ワードに該当するデータがある場合' do
       it 'データが取得できること' do
         VCR.use_cassette('requests/series_playlists/search_return_results') do
-          get search_series_playlists_path
+          get search_series_playlists_path, params: { word: 'カムカムエヴリバディ' }
 
           expect(response.status).to eq 200
           json = JSON.parse(response.body)
-          expect(json['count']).not_to eq 0
+          expect(json['count']).not_to eq 0 # 件数が変動する可能性があるため、0件でないことをチェック
         end
       end
     end
@@ -55,6 +55,7 @@ describe SeriesPlaylist, type: :request do
           expect(response.status).to eq 200
           json = JSON.parse(response.body)
           expect(json['count']).to eq 0
+          # エラーメッセージはフロント側でハンドリングしているためテストに含めない
         end
       end
     end
