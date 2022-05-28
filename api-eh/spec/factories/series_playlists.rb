bf6948a977a64f6142048d1387b687110b00f376
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-05-29 01:17:33 +0900

    fix：Factoryから不要なカラムのデータを削除

--- api-eh/spec/factories/series_playlists.rb
+++ api-eh/spec/factories/series_playlists.rb
@@ -4,10 +4,6 @@ FactoryBot.define do
   factory :series_playlist do
     author_name { 'オウサム　ネーム' }
     headline { 'ヘッドラインのテキストが入ります' }
-    marked_header { 'ヘッダのテキストが入ります' }
-    article_body { 'ボディのテキストが入ります' }
-    marked_footer { 'フッタのテキストが入ります' }
-    available_article { [true, false].sample }
     active_article { [true, false].sample }
   end
 end
