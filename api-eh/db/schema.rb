f130f0b3f11a08c732d4a12d86b9a359b34f7b7e
norikt <norikt@gmail.com>
2022-06-01 17:18:15 +0900

    Run bin/rails app:update and fix

--- api-eh/db/schema.rb
+++ api-eh/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_060119) do
+ActiveRecord::Schema[6.1].define(version: 2022_05_20_060119) do
 
   create_table "article_images", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "playlist_id", comment: "プレイリストID"
