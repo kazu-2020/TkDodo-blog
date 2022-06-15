eec34daf75fb010cebc1ed6ea403d5c052836e19
Eigoro Yamamura <yamamura@sikmi.com>
2022-06-15 13:51:42 +0900

    シリーズデッキのテストを追加

--- e2e/cypress/integration/series_deck/001_series_deck_new_spec.js
+++ e2e/cypress/integration/series_deck/001_series_deck_new_spec.js
@@ -5,9 +5,57 @@ describe('シリーズデッキ新規作成', () => {
     cy.visit('/')
     cy.contains('デッキ').click()
     cy.contains('シリーズデッキ新規作成').click()
+
+    // リスト（playlist）の編集
+    // リストの追加はAPIが関連するため実装をスキップ
+    // cy.get('td .add_button').first().click()
+
+    // 基本情報（deck）の編集
+    cy.get('.series-step').click()
+
+    const deckName = `${now}シリーズデッキ`
+    cy.get('.deck_name input[type="text"]').eq(0).type(deckName)
+    const interfix = Date.now()
+    cy.get('.deck_name input[type="text"]').eq(1).type(interfix)
+
+    cy.get('textarea[name=catch]').type(deckName + interfix)
+
+    cy.contains('保存する').click({ force: true })
+
+    cy.waitLoading()
+
+    // 登録内容の確認
+    // 基本情報(NSeries)の確認
+    cy.get('.series-step').click()
+
+    cy.get('.deck_name input[type="text"]').eq(0).should(
+      'have.value',
+      deckName
+    )
+
+    cy.get('.deck_name input[type="text"]').eq(1).should(
+      'have.value',
+      interfix
+    )
+
+    cy.get('textarea[name=catch]').should(
+      'have.value',
+      deckName + interfix
+    )
   })
 
   it('新規作成したシリーズデッキが検索できること', () => {
+    const now = Cypress.env('NOW')
 
+    cy.visit('/')
+    cy.contains('デッキ').click()
+    cy.contains('シリーズデッキ一覧').click()
+
+    cy.waitLoading()
+    cy.wait(500)
+
+    // 対象のデッキが表示されていること
+    cy.get('.row').eq(2).contains(now).should('have.lengthOf', 1)
+    cy.get('.row').eq(2).contains('非公開').should('have.lengthOf', 1)
   })
 })
--- e2e/cypress/integration/series_deck/002_series_deck_update_spec.js
+++ e2e/cypress/integration/series_deck/002_series_deck_update_spec.js
@@ -1,9 +1,66 @@
 describe('シリーズデッキ更新', () => {
   it('シリーズデッキを選択し、メタの更新をする', () => {
+    const now = Cypress.env('NOW')
 
+    cy.visit('/')
+    cy.contains('デッキ').click()
+    cy.contains('シリーズデッキ一覧').click()
+
+    cy.waitLoading()
+    cy.wait(500)
+
+    cy.get('.deck-name').contains(now).click()
+
+    cy.waitLoading()
+
+    cy.get('.v-navigation-drawer .edit_button.orange').click()
+
+    // 基本情報（deck）の編集
+    cy.get('.series-step').click()
+
+    const deckName = `${now}シリーズデッキ2`
+    cy.get('.deck_name input[type="text"]').eq(0).clear().type(deckName)
+    const interfix = Date.now()
+    cy.get('.deck_name input[type="text"]').eq(1).clear().type(interfix)
+    cy.get('textarea[name=catch]').clear().type(deckName + interfix + "2")
+    cy.get('label').contains('公開する').click()
+
+    cy.contains('保存する').click({ force: true })
+
+    cy.waitLoading()
+
+    // 登録内容の確認
+    // 基本情報(NSeries)の確認
+    cy.get('.series-step').click()
+
+    cy.get('.deck_name input[type="text"]').eq(0).should(
+      'have.value',
+      deckName
+    )
+
+    cy.get('.deck_name input[type="text"]').eq(1).should(
+      'have.value',
+      interfix
+    )
+
+    cy.get('textarea[name=catch]').should(
+      'have.value',
+      deckName + interfix + "2"
+    )
   })
 
   it('更新したシリーズデッキが検索できること', () => {
+    const now = Cypress.env('NOW')
 
+    cy.visit('/')
+    cy.contains('デッキ').click()
+    cy.contains('シリーズデッキ一覧').click()
+
+    cy.waitLoading()
+    cy.wait(500)
+
+    // 対象のデッキが表示されていること
+    cy.get('.row').eq(2).contains(now).should('have.lengthOf', 1)
+    cy.get('.row').eq(2).contains('公開中').should('have.lengthOf', 1)
   })
 })
