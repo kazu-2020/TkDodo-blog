42004b3731b1a113805ab8f96cbb18a4ffd9be57
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-06-21 12:41:26 +0900

    エピソード検索ロジックをSearchクラスに移行

--- api-eh/app/services/search_episodes.rb
+++ api-eh/app/services/search_episodes.rb
@@ -1,4 +1,11 @@
 class SearchEpisodes
+  DEFAULT_OFFSET = 0
+  DEFAULT_SIZE = 10
+  DEFAULT_TYPE = 'tvepisode'.freeze
+  DEFAULT_SORT_ORDER = 'desc'.freeze
+  DEFAULT_SORT_ORDER_BY = 'score'.freeze
+  QUERY_KEYS = %i[word concern keyword service].freeze
+
   def call(client, search_params)
     episode(client, search_params)
   end
@@ -6,7 +13,9 @@ class SearchEpisodes
   private
 
   def episode(client, search_params)
-    result = client.search(merged_params: search_params,
+    merged_params = merge_params(search_params)
+
+    result = client.search(merged_params: merged_params,
                            query: { publishLevel: 'notyet,ready,full,limited,gone' })&.deep_symbolize_keys
     # okushibu3のために、r6.0からEpisodeを引き直して検索結果に設定し直している
     result.dig(:result, :tvepisode, :result).each do |episode|
@@ -14,5 +23,35 @@ class SearchEpisodes
     end
 
     result
+  end
+
+  # パラメータをマージする
+  #
+  # @param [Hash] search_params
+  def merge_params(search_params)
+    offset = search_params[:offset] || DEFAULT_OFFSET
+    ignore_range = search_params[:ignore_range].nil? ? true : search_params[:ignore_range]
+    sort_order = search_params[:order] || DEFAULT_SORT_ORDER
+    sort_order_by = search_params[:order_by] || DEFAULT_SORT_ORDER_BY
+    size = search_params[:size] || DEFAULT_SIZE
+    merged_params = { offset: offset, isFuzzy: true, ignoreRange: ignore_range,
+                      order: sort_order, orderBy: sort_order_by, size: size }
+    merged_params.merge!(search_query_hash(search_params))
+
+    merged_params
+  end
+
+  # クエリをマージする
+  #
+  # @param [Hash] search_params
+  def search_query_hash(search_params)
+    merged_params = {}
+    search_params.each_key do |k|
+      next unless QUERY_KEYS.include?(k.to_sym)
+
+      merged_params.merge!("#{k}": search_params[k.to_sym]) if search_params[k.to_sym].present?
+    end
+
+    merged_params
   end
 end
