a402a10985b5a0f9730f591cd50f91bffe3b4198
norikt <norikt@gmail.com>
2022-07-05 14:45:31 +0900

    デッキの管理設定のルーティングを追加

---
+++ frontend-edge/src/features/recommend-decks/routes/ConfigRecommendDeck.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const ConfigRecommendDeck = () => <h3>ConfigRecommendDeck</h3>
+export default ConfigRecommendDeck
---
+++ frontend-edge/src/features/series-decks/routes/ConfigSeriesDeck.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const ConfigSeriesDeck = () => <h3>ConfigSeriesDeck</h3>
+export default ConfigSeriesDeck
