99e84198a08f903a1bae318e6cdc47dbc3f758e2
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-07-07 16:00:36 +0900

    UIの調整

--- frontend/components/decks/DeckPlaylists.vue
+++ frontend/components/decks/DeckPlaylists.vue
@@ -1,17 +1,29 @@
 <template>
-  <v-simple-table>
-    <template #default>
-      <thead>
-        <tr>
-          <th class="pr-0" />
-          <th class="text-left pl-0" colspan="2">プレイリスト</th>
-          <th class="text-left">記事の有無</th>
-          <th class="text-left">視聴可能エピソード数</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="playlist in playlists" :key="playlist.playlistUId">
-          <td width="30px">
+  <div>
+    <v-simple-table>
+      <template #default>
+        <thead>
+          <tr>
+            <th class="pr-0" width="83" />
+            <th class="text-left pr-0 pl-0" width="340">プレイリスト</th>
+            <th class="text-left pr-0 pl-0" width="310">記事の有無</th>
+            <th class="text-left pr-0 pl-0">視聴可能エピソード数</th>
+          </tr>
+        </thead>
+      </template>
+    </v-simple-table>
+    <v-expansion-panels accordion tile focusable>
+      <v-expansion-panel
+        v-for="playlist in playlists"
+        :key="playlist.playlistUId"
+        v-model="isExpanded"
+        @click="fetchEpisodes(playlist)"
+      >
+        <v-expansion-panel-header>
+          <template #actions>
+            <v-icon color="#3498db"> mdi-menu-down </v-icon>
+          </template>
+          <td class="delete-button pr-5">
             <v-btn
               tile
               color="orange"
@@ -21,10 +33,10 @@
               min-width="21px"
               @click="deletePlaylist(playlist)"
             >
-              <v-icon> mdi-minus</v-icon>
+              <v-icon> mdi-minus </v-icon>
             </v-btn>
           </td>
-          <td class="pl-0" width="30px">
+          <td class="playlist-image">
             <v-img
               :src="logoUrl(playlist)"
               lazy-src="https://placehold.jp/40x40.png"
@@ -47,22 +59,61 @@
               >{{ playlist.playableEpisodesCount }}/{{ playlist.itemNum }}
             </v-chip>
             <v-chip v-else class="mx-2" color="grey" label text-color="white"
-              >0/{{ playlist.itemNum }}
-            </v-chip>
+              >0/{{ playlist.itemNum }}</v-chip
+            >
           </td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+          <span class="display-episode">エピソード表示</span>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content class="pl-16">
+          <div v-if="isFetched">
+            <div v-if="playlist.itemNum !== 0">
+              <div
+                v-for="episode in filterPlaylistEpisodes(playlist)"
+                :key="`deck-playlist-list-item-ep-${episode.id}`"
+              >
+                <v-img
+                  :src="eyeCatchUrl(episode)"
+                  lazy-src="https://placehold.jp/100x56.png"
+                  width="44"
+                  height="24"
+                  class="episode-image float-left mr-1"
+                />
+              </div>
+            </div>
+            <div v-else>
+              <v-row align="center" justify="center"
+                ><v-col class="body-2">エピソードはありません</v-col></v-row
+              >
+            </div>
+          </div>
+          <div v-else align="center" class="height-100">
+            <v-row v-if="isError" align="center" justify="center"
+              ><v-col class="body-2">エラーが発生しました</v-col></v-row
+            >
+            <v-row v-else align="center" justify="center"
+              ><v-col><v-progress-circular indeterminate color="amber" /></v-col
+            ></v-row>
+          </div>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 interface DataType {
-  headers: object
+  isError: boolean
+  isFetched: boolean
+  isExpanded: []
+  playlistEpisodes: [
+    {
+      playlistUId: string
+      episodes: []
+    }
+  ]
 }
-
 export default Vue.extend({
   name: 'DeckPlaylist',
   props: {
@@ -74,15 +125,10 @@ export default Vue.extend({
   },
   data(): DataType {
     return {
-      headers: [
-        {
-          value: 'name',
-          text: 'プレイリスト',
-          sortable: false,
-        },
-        { value: 'articleStatus', text: '記事の有無', sortable: false },
-        { value: 'videoStatus', text: '視聴可能エピソード数', sortable: false },
-      ],
+      isError: false,
+      isFetched: false,
+      isExpanded: [],
+      playlistEpisodes: [{ playlistUId: '', episodes: [] }],
     }
   },
   computed: {
@@ -110,6 +156,17 @@ export default Vue.extend({
 
       return 'https://placehold.jp/40x40.png'
     },
+    eyeCatchUrl(episode: any) {
+      if (episode?.eyecatch !== undefined) {
+        return episode.eyecatch.medium.url
+      } else if ((episode?.keyvisuals || [])[0] !== undefined) {
+        return episode.keyvisuals[0].small.url
+      } else if (episode?.partOfSeries?.eyecatch !== undefined) {
+        return episode.partOfSeries.eyecatch.medium.url
+      }
+
+      return 'https://placehold.jp/100x56.png'
+    },
     hasVideo(playlist: any) {
       return playlist.playableEpisodesCount !== 0
     },
@@ -123,6 +180,78 @@ export default Vue.extend({
         return '×'
       }
     },
+    fetchEpisodes(playlist: any) {
+      this.isFetched = false
+      let isAlreadyAdded = false
+
+      for (const item of this.playlistEpisodes) {
+        if (item.playlistUId === playlist.playlistUId) {
+          isAlreadyAdded = true
+          break
+        }
+      }
+
+      if (isAlreadyAdded) {
+        this.isFetched = true
+        return
+      }
+
+      this.$axios
+        .get(`/playlists/${playlist.playlistUId}/playlist_items?limit=10`, {
+          headers: {
+            Authorization: `Bearer ${this.$store.state.token}`,
+          },
+        })
+        .then((res) => {
+          this.playlistEpisodes.push({
+            playlistUId: playlist.playlistUId,
+            episodes: res.data.items,
+          })
+        })
+        .catch(() => {
+          this.isError = true
+        })
+        .finally(() => {
+          this.isFetched = true
+        })
+    },
+    filterPlaylistEpisodes(playlist: any): any {
+      for (const playlistEpisode of this.playlistEpisodes) {
+        if (playlistEpisode.playlistUId === playlist.playlistUId) {
+          return playlistEpisode.episodes
+        }
+      }
+    },
   },
 })
 </script>
+
+<style lang="scss" scoped>
+.delete-button.v-btn.v-btn--tile.v-size--small {
+  min-width: 0;
+  padding: 0 2px;
+}
+.v-expansion-panel-header {
+  height: 35px;
+}
+.display-episode {
+  text-align: right;
+  color: #3498db;
+}
+.delete-button,
+.playlist-image {
+  flex: 0 0 30px;
+}
+.playlist-name,
+.playlist-can-be-watch,
+.playlist-status,
+.display-episode {
+  flex: 1 1 300px;
+}
+.v-expansion-panels {
+  z-index: auto !important;
+}
+.v-expansion-panel-content::v-deep .v-expansion-panel-content__wrap {
+  padding-top: 16px;
+}
+</style>
