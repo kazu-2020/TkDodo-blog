a6d0ec888b6c3f969d44b698d86d8774f605c3bf
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-07-20 19:18:38 +0900

    外部キーにNULL制約を付与

--- api-eh/db/migrate/20220720015715_create_supervisors.rb
+++ api-eh/db/migrate/20220720015715_create_supervisors.rb
@@ -1,8 +1,8 @@
 class CreateSupervisors < ActiveRecord::Migration[7.0]
   def change
     create_table :supervisors do |t|
-      t.references :person_organization_locals, foreign_key: true
-      t.references :playlist, foreign_key: true
+      t.references :person_organization_locals, foreign_key: true, null: false
+      t.references :playlist, foreign_key: true, null: false
       t.json :image_data
       t.string :type
       t.bigint :type_id
