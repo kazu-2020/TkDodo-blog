5b7d46695a6e4c5dd847a94c9438370d1c6dd8bb
norikt <norikt@gmail.com>
2022-07-22 17:49:40 +0900

    コンテンツ幅を広くしてとパンくずを揃える

--- frontend-edge/src/components/Layout.tsx
+++ frontend-edge/src/components/Layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom'
 import React from 'react'
 import { Flex, ChakraProvider, Container } from '@chakra-ui/react'
 
-
 import theme from '../lib/theme'
 
 import Header from './Header'
@@ -13,12 +12,12 @@ const Layout = () => (
   <ChakraProvider theme={theme}>
     <Flex direction="column" minH="100vh">
       <Header />
-      <BreadcrumbContextProvider>
-        <Breadcrumb mt="64px" />
-        <Container maxW="container.lg" py={4} flexGrow="1" mt="16px" mb="32px">
+      <Container maxW="container.xl" flexGrow="1" mt="64px" mb="32px">
+        <BreadcrumbContextProvider>
+          <Breadcrumb my={5} />
           <Outlet />
-        </Container>
-      </BreadcrumbContextProvider>
+        </BreadcrumbContextProvider>
+      </Container>
       <Footer />
     </Flex>
   </ChakraProvider>
