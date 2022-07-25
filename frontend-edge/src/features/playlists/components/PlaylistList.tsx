a8ffb298f8918690e0d3713d1e7af28a83920e4b
Eigoro Yamamura <yamamura@sikmi.com>
2022-07-25 17:03:54 +0900

    プレイリスト一覧の検索処理を実装

--- frontend-edge/src/features/playlists/components/PlaylistList.tsx
+++ frontend-edge/src/features/playlists/components/PlaylistList.tsx
@@ -1,42 +1,62 @@
-import { useState } from 'react'
-import { Skeleton, Stack, useDisclosure } from '@chakra-ui/react'
+import { ChangeEvent, useState } from 'react'
+import { Flex, Grid, GridItem, Stack } from '@chakra-ui/react'
 
-import { Playlist } from '@/types/playlist'
-import PlaylistListItem from '@/features/playlists/components/PlaylistListItem'
-import PlaylistDrawer from '@/features/playlists/components/PlaylistDrawer'
+import PlaylistListItems from '@/features/playlists/components/PlaylistListItems'
+import ArticleModeSwitcher from '@/features/playlists/components/ArticleModeSwitch'
+import SearchTextInput from '@/features/misc/components/SearchTextInput'
+import ListScreenSkeleton from '@/features/misc/components/ListScreenSkeleton'
+import APIStatusSelect from '@/features/misc/components/APIStatusSelect'
 
 import { usePlaylists } from '../api/getPlaylists'
 
 const PlaylistList = () => {
-  const { data, isLoading } = usePlaylists()
-  const [selectedPlaylist, setSelectedPlaylist] = useState<
-    Playlist | undefined
-  >(undefined)
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [page, setPage] = useState(1)
+  const [query, setQuery] = useState('')
+  const [apiState, setApiState] = useState('open')
+
+  const { data, isLoading } = usePlaylists({
+    query,
+    apiState,
+    page
+  })
 
   if (isLoading) {
-    return (
-      <Stack>
-        {[...Array(20)].map((_, i) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Skeleton key={i} h="48px" px={3} bg="white" borderRadius="md" />
-        ))}
-      </Stack>
-    )
+    return <ListScreenSkeleton />
   }
+
+  const playlists = data?.playlists
+  const totalCount = data?.pagination?.count || 0
 
   return (
     <Stack>
-      {data?.map((playlist: Playlist) =>
-        PlaylistListItem({ playlist, setSelectedPlaylist, onOpen })
-      )}
-      {selectedPlaylist && (
-        <PlaylistDrawer
-          playlist={selectedPlaylist}
-          isOpen={isOpen}
-          onClose={onClose}
-        />
-      )}
+      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        <GridItem colSpan={2}>
+          <SearchTextInput
+            placeholder="プレイリストタイトルで検索"
+            onAction={setQuery}
+          />
+        </GridItem>
+
+        <GridItem colStart={4} colEnd={6}>
+          <Flex justify="flex-end">
+            {/* 記事モード作成までは無効化 */}
+            {false && <ArticleModeSwitcher />}
+
+            <APIStatusSelect
+              onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+                setApiState(event.target.value)
+              }}
+            />
+          </Flex>
+        </GridItem>
+      </Grid>
+
+      <PlaylistListItems
+        items={playlists}
+        page={page}
+        totalCount={totalCount}
+        onChangePage={setPage}
+      />
     </Stack>
   )
 }
