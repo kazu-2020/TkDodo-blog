9aa66726d90c40f85fdf50bbfafccc07e92a7f39
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-07-26 22:11:45 +0900

    ディレクトリ配下のファイルを読み込めるように修正

--- api-eh/db/seeds.rb
+++ api-eh/db/seeds.rb
@@ -1,16 +1,5 @@
-tables_name = %w[
-  person_organization_locals supervisors wikidata_jsons viaf_jsons person_organization_globals
-  search_persons_organizations wikidata_properties wikidata_same_as
-]
-
-# 初期データ投入
-prefix = 'input_init_data_to_'
-tables_name.each do |table_name|
-  path = Rails.root.join("db/fixtures/#{Rails.env}/#{prefix}#{table_name}.rb")
-  if File.exist?(path)
-    puts "Seeding： #{table_name}"
-    require path
-  else
-    puts "Seeding： #{prefix}#{table_name} が存在しません"
-  end
+Dir.glob(File.join(Rails.root, 'db', 'fixtures', "#{Rails.env}", '*.rb')).each do |file|
+  puts "Seeding： #{file}"
+  load(file)
+  puts "Seeded： #{file}"
 end
