791fd998b1e57c718a4efa8a61577a3948f815ae
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-07-27 17:25:38 +0900

    seed_fuの導入

--- api-eh/db/seeds.rb
+++ api-eh/db/seeds.rb
@@ -1,9 +1,2 @@
-begin
-  Dir.glob(File.join(Rails.root, 'db', 'fixtures', Rails.env.to_s, '*.rb')).each do |file|
-    puts "Seeding： #{file}"
-    load(file)
-    puts "Seeded： #{file}"
-  end
-rescue LoadError => e
-  puts e.to_s
-end
+# rubocop:disable Lint/EmptyFile, Lint/RedundantCopDisableDirective
+# rubocop:enable Lint/EmptyFile, Lint/RedundantCopDisableDirective
