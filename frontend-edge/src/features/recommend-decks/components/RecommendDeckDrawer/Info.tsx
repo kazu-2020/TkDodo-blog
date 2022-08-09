74dc2c6b54e880125b7fa58a9a5e452ae45d60c0
Eigoro Yamamura <yamamura@sikmi.com>
2022-08-09 16:26:52 +0900

    レコメンドデッキのドロワーの実装

---
+++ frontend-edge/src/features/recommend-decks/components/RecommendDeckDrawer/Info.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Box, Text, useToast, VStack } from '@chakra-ui/react'
+
+import { Deck } from '@/types/deck'
+import { TextCopyBadge } from '@/components/TextCopyBadge'
+import ApiStateBadge from '@/components/ApiStateBadge'
+
+type Props = {
+  recommendDeck: Deck
+}
+
+export const Info = ({ recommendDeck }: Props) => {
+  const toast = useToast()
+  const onCopy = () => {
+    toast({
+      title: 'コピー',
+      description: 'IDをコピーしました',
+      status: 'info',
+      duration: 3000,
+      position: 'bottom-right',
+      isClosable: true
+    })
+  }
+
+  return (
+    <VStack
+      align="flex-start"
+      spacing={4}
+      borderWidth={1}
+      borderRadius="sm"
+      m={4}
+      p={3}
+    >
+      <Text fontWeight={700}>{recommendDeck.name}</Text>
+      <Box>
+        <ApiStateBadge apiState={recommendDeck.apiState} />
+        <TextCopyBadge text={recommendDeck.stringId} onCopy={onCopy} />
+      </Box>
+      {recommendDeck.description && (
+        <Box>
+          <Text fontWeight={700} fontSize="sm">
+            説明
+          </Text>
+          <Text fontSize="sm">{recommendDeck.description}</Text>
+        </Box>
+      )}
+    </VStack>
+  )
+}
