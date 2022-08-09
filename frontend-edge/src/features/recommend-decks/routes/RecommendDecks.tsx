12289e37387bbf40db51bdad49763774e3d05d03
Eigoro Yamamura <yamamura@sikmi.com>
2022-08-09 10:54:08 +0900

    レコメンドデッキの一覧を追加

--- frontend-edge/src/features/recommend-decks/routes/RecommendDecks.tsx
+++ frontend-edge/src/features/recommend-decks/routes/RecommendDecks.tsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import { Heading } from '@chakra-ui/react'
 
-const RecommendDecks = () => <h3>RecommendDecks</h3>
+import RecommendDeckList from '../components/RecommendDeckList'
+
+const RecommendDecks = () => (
+  <>
+    <Heading as="h2" size="md" mb={5}>
+      レコメンドデッキ一覧
+    </Heading>
+    <RecommendDeckList />
+  </>
+)
 export default RecommendDecks
