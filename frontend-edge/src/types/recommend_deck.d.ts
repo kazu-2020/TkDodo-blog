92b58147c12c64b68e4c9a5c685083f17017eb7a
Eigoro Yamamura <yamamura@sikmi.com>
2022-08-09 10:53:41 +0900

    レコメンドデッキの型を追加

---
+++ frontend-edge/src/types/recommend_deck.d.ts
@@ -0,0 +1,15 @@
+import { SeriesPlaylist } from './series_playlist'
+
+export type RecommendDeck = {
+  id: string
+  name: string
+  description?: string
+  interfix: string
+  stringId: string
+  adminMemo?: string
+  apiState: 'open' | 'close' | 'waiting'
+  playListCount: number
+  playlists: SeriesPlaylist[]
+  dateCreated: string
+  dateModified: string
+}
