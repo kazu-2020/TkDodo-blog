8ccbf7562019a3d99c913cf71e9e6b0b076d2460
norikt <norikt@gmail.com>
2022-08-16 17:51:04 +0900

    シリーズデッキ編集のStoryを追加

---
+++ frontend-edge/src/features/series-decks/routes/SeriesDeck.stories.tsx
@@ -0,0 +1,40 @@
+import { QueryClientProvider } from 'react-query'
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { Box } from '@chakra-ui/react'
+
+import { handlers } from '@/test/server/handlers'
+import { db, resetDb } from '@/test/server/db'
+import { seriesDeckGenerator } from '@/test/data-generators'
+import { queryClient } from '@/lib/react-query'
+
+import SeriesDeck from './SeriesDeck'
+
+export default {
+  component: SeriesDeck
+} as ComponentMeta<typeof SeriesDeck>
+
+const fakeSeriesDeck = seriesDeckGenerator()
+
+export const Default: ComponentStoryObj<typeof SeriesDeck> = {
+  render: () => (
+    <QueryClientProvider client={queryClient}>
+      {/* ヘッダー分の高さを設定してStickyなArrowStepを調整 */}
+      <Box h="60px" bgColor="#f0f0f0" zIndex="888" />
+      <SeriesDeck />
+    </QueryClientProvider>
+  ),
+  parameters: {
+    msw: {
+      handlers
+    },
+    reactRouter: {
+      routePath: '/series-decks/:seriesDeckId',
+      routeParams: { seriesDeckId: fakeSeriesDeck.id }
+    }
+  },
+  play: async () => {
+    // NOTE: NewSeriesDeckと被るのでsmoke testのみ
+    await resetDb()
+    await db.seriesDeck.create(fakeSeriesDeck)
+  }
+}
