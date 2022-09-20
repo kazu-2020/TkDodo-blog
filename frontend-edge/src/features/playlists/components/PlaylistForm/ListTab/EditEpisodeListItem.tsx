cda67a74e3e43e31fc0b941029f6ce925c3868a1
Eigoro Yamamura <yamamura@sikmi.com>
2022-09-20 11:37:58 +0900

    utilへ置き換え

--- frontend-edge/src/features/playlists/components/PlaylistForm/ListTab/EditEpisodeListItem.tsx
+++ frontend-edge/src/features/playlists/components/PlaylistForm/ListTab/EditEpisodeListItem.tsx
@@ -1,12 +1,10 @@
 import { HiOutlineMinus } from 'react-icons/all'
 import { UseFormSetValue } from 'react-hook-form/dist/types/form'
-import { useFormContext, UseFormGetValues, useWatch } from 'react-hook-form'
+import { useFormContext, UseFormGetValues } from 'react-hook-form'
 import React from 'react'
-import { differenceInMilliseconds } from 'date-fns'
 import {
   Button,
   Center,
-  Flex,
   Grid,
   GridItem,
   HStack,
@@ -16,52 +14,14 @@ import {
 import { Icon } from '@chakra-ui/icons'
 
 import { hasVideo } from '@/utils/video'
-import { formatDateWithWeekday } from '@/utils/format'
+import { episodeThumbnailUrl } from '@/utils/image'
 import { EpisodeData } from '@/types/episode_data'
+import {
+  resentEventStartDate,
+  totalTime
+} from '@/features/playlists/utils/episodeFormat'
 import { PlaylistFormInputs } from '@/features/playlists/types'
 import { PlayableStatusBadge } from '@/components/PlayableStatusBadge'
-import { episodeThumbnailUrl } from '@/utils/image'
-
-const eyecatchUrl = (episode: EpisodeData) => {
-  if (episode.eyecatch !== undefined) {
-    return episode.eyecatch.medium?.url
-  }
-  if (episode.keyvisuals !== undefined) {
-    return episode.keyvisuals[0]?.small?.url
-  }
-  if (episode.partOfSeries?.eyecatch !== undefined) {
-    return episode.partOfSeries.eyecatch.medium?.url
-  }
-
-  return 'https://placehold.jp/71x40.png'
-}
-
-const seriesName = (episode: EpisodeData) => episode?.partOfSeries?.name || ''
-
-const totalTime = (episode: EpisodeData) => {
-  if (episode.detailedRecentEvent === undefined) return '--:--:--'
-  const totalSecond =
-    differenceInMilliseconds(
-      new Date(episode.detailedRecentEvent.endDate),
-      new Date(episode.detailedRecentEvent.startDate)
-    ) / 1000
-
-  const seconds = totalSecond % 60
-  const totalMinutes = (totalSecond - seconds) / 60
-  const minutes = totalMinutes % 60
-  const hours = Math.floor(totalMinutes / 60)
-
-  return `${`00${hours}`.slice(-2)}:${`00${minutes}`.slice(
-    -2
-  )}:${`00${seconds}`.slice(-2)}`
-}
-
-const resentEventStartDate = (episode: EpisodeData) => {
-  const startDate = episode.detailedRecentEvent?.startDate
-  if (startDate === undefined) return ''
-
-  return formatDateWithWeekday(startDate)
-}
 
 const removeEpisode = (
   getValues: UseFormGetValues<PlaylistFormInputs>,
