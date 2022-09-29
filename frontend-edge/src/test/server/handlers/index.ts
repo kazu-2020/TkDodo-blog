e55b8d351a2e0e10b4f94d5d49e5c7585773810a
norikt <norikt@gmail.com>
2022-09-29 17:01:04 +0900

    ActiveCheckboxesのstorybookを追加

--- frontend-edge/src/test/server/handlers/index.ts
+++ frontend-edge/src/test/server/handlers/index.ts
@@ -1,10 +1,12 @@
 import { recommendDecksHandlers } from '@/test/server/handlers/recommendDecks'
+import { bundleItemsHandlers } from '@/test/server/handlers/bundleItems'
 
 import { seriesPlaylistsHandlers } from './seriesPlaylists'
 import { seriesDecksHandlers } from './seriesDecks'
 import { playlistsHandlers } from './playlists'
 
 export const handlers = [
+  ...bundleItemsHandlers,
   ...playlistsHandlers,
   ...seriesDecksHandlers,
   ...recommendDecksHandlers,
