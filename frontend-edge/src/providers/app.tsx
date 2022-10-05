9d506b512fd93a422a8957316bf73c16aad32be2
Eigoro Yamamura <yamamura@sikmi.com>
2022-10-05 15:01:31 +0900

    エラーページの追加

--- frontend-edge/src/providers/app.tsx
+++ frontend-edge/src/providers/app.tsx
@@ -3,26 +3,12 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import { QueryClientProvider } from 'react-query'
 import { ErrorBoundary } from 'react-error-boundary'
 import * as React from 'react'
-import { Button, CircularProgress } from '@chakra-ui/react'
+import { CircularProgress } from '@chakra-ui/react'
 
+import { ErrorFallback } from '@/providers/ErrorFallback'
 import { queryClient } from '@/lib/react-query'
 import { Auth0ProviderWithRedirectCallback } from '@/lib/auth0/Auth0ProviderWithRedirectCallback'
 import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from '@/config'
-
-const ErrorFallback = () => (
-  <div
-    className="text-red-500 w-screen h-screen flex flex-col justify-center items-center"
-    role="alert"
-  >
-    <h2 className="text-lg font-semibold">Ooops, something went wrong :( </h2>
-    <Button
-      className="mt-4"
-      onClick={() => window.location.assign(window.location.origin)}
-    >
-      Refresh
-    </Button>
-  </div>
-)
 
 type AppProviderProps = {
   children: React.ReactNode
