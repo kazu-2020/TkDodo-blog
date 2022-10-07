92f38557d3ba13f5aa619c0c2f218ac4aac3191b
Eigoro Yamamura <yamamura@sikmi.com>
2022-10-07 15:53:57 +0900

    レコメンドデッキのプレイリスト検索にページングと検索フォームを追加

--- frontend-edge/src/features/recommend-decks/components/RecommendDeckForm/RecommendPlaylistList.tsx
+++ frontend-edge/src/features/recommend-decks/components/RecommendDeckForm/RecommendPlaylistList.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Box, HStack, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Box, Text } from '@chakra-ui/react'
 
 import { RecommendPlaylist } from '@/types/recommend_playlist'
+import { SearchResultLoadMoreButton } from '@/features/series-decks/components/SeriesDeckForm'
 import { useRecommendDeckFormStore } from '@/features/recommend-decks/stores/recommendDeckForm'
 import { RecommendPlaylistListItem } from '@/features/recommend-decks/components/RecommendDeckForm/RecommendPlaylistListItem'
 import { RecommendPlaylistListHeader } from '@/features/recommend-decks/components/RecommendDeckForm/RecommendPlaylistListHeader'
 import { usePlaylists } from '@/features/recommend-decks/api/getPlaylists'
+import { SearchTextInput } from '@/components/SearchTextInput'
 import { ListScreenSkeleton } from '@/components/ListScreenSkeleton'
+
+const PER_PAGE = 10
 
 export const RecommendPlaylistList = () => {
   const { recommendPlaylists, addRecommendPlaylist } =
@@ -14,11 +18,14 @@ export const RecommendPlaylistList = () => {
       recommendPlaylists: state.recommendPlaylists,
       addRecommendPlaylist: state.addRecommendPlaylist
     }))
+  const [query, setQuery] = useState('')
 
-  const { data, isLoading } = usePlaylists({
-    per: 1000,
-    withEpisodeCount: 1
-  })
+  const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    usePlaylists({
+      query,
+      per: PER_PAGE,
+      withEpisodeCount: 1
+    })
 
   function hasRecommendPlaylist(playlist: RecommendPlaylist) {
     return recommendPlaylists.some(
@@ -26,28 +33,42 @@ export const RecommendPlaylistList = () => {
     )
   }
 
+  const onAction = (q: string) => {
+    setQuery(q)
+  }
+
   return (
     <Box>
-      <HStack justifyContent="space-between">
-        <Text>編成可能なプレイリスト</Text>
-        {!isLoading && data && <Text>全{data.pagination.count}件</Text>}
-      </HStack>
+      <SearchTextInput
+        placeholder="プレイリストを検索する"
+        onAction={onAction}
+      />
       {/* 検索結果 */}
-      {isLoading && <ListScreenSkeleton size={10} />}
+      {isLoading && <ListScreenSkeleton size={PER_PAGE} />}
       {!isLoading && !data && <Text>編成可能なプレイリストがありません</Text>}
       {!isLoading && data && (
         <Box py={3}>
+          <Text>全{data?.pages?.at(0)?.pagination.count}件</Text>
           <RecommendPlaylistListHeader />
           <Box data-testid="recommend-playlist-search-results">
-            {data?.playlists?.map((playlist: RecommendPlaylist) => (
-              <RecommendPlaylistListItem
-                key={playlist.playlistUId}
-                hasRecommendPlaylist={hasRecommendPlaylist(playlist)}
-                playlist={playlist}
-                onClick={() => addRecommendPlaylist(playlist)}
-              />
-            ))}
+            {data?.pages.map(({ playlists }) =>
+              playlists?.map((playlist: RecommendPlaylist) => (
+                <RecommendPlaylistListItem
+                  key={playlist.playlistUId}
+                  hasRecommendPlaylist={hasRecommendPlaylist(playlist)}
+                  playlist={playlist}
+                  onClick={() => addRecommendPlaylist(playlist)}
+                />
+              ))
+            )}
           </Box>
+          {hasNextPage && (
+            <SearchResultLoadMoreButton
+              perPage={PER_PAGE}
+              onClick={() => fetchNextPage()}
+              isLoading={isFetchingNextPage}
+            />
+          )}
         </Box>
       )}
     </Box>
---
+++ frontend-edge/src/features/recommend-decks/components/RecommendDeckForm/RecommendPlaylistLoadMoreButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button, ButtonProps, Grid, GridItem } from '@chakra-ui/react'
+
+export const RecommendPlaylistLoadMoreButton = ({
+  perPage,
+  isLoading,
+  onClick,
+  loadingText
+}: { perPage: number } & ButtonProps) => (
+  <Grid templateColumns="repeat(12, 1fr)" color="gray" pb={2}>
+    <GridItem colSpan={12} h="5">
+      <Button
+        onClick={onClick}
+        w="100%"
+        bgColor="white"
+        color="primary"
+        borderRadius="none"
+        isLoading={isLoading}
+        loadingText={loadingText || '取得中'}
+      >
+        次の{perPage}件を表示
+      </Button>
+    </GridItem>
+  </Grid>
+)
