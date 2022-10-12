0dfbca5b1fd94ebac48324ed24527f05f482fd3d
norikt <norikt@gmail.com>
2022-10-12 14:16:50 +0900

    Bump cypress 10.9 to 10.10

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -24,6 +24,7 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+import "cypress-real-events/support"
 import 'cypress-file-upload'
 
 
