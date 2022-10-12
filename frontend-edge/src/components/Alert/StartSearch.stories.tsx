8f93df7d30b25978ed69315636ffe666b674eebe
Eigoro Yamamura <yamamura@sikmi.com>
2022-10-12 16:36:07 +0900

    検索前のアラート用コンポーネントの追加

---
+++ frontend-edge/src/components/Alert/StartSearch.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+
+import { StartSearch } from '@/components/Alert/StartSearch'
+
+export default {
+  component: StartSearch
+} as ComponentMeta<typeof StartSearch>
+
+export const Default: ComponentStoryObj<typeof StartSearch> = {}
