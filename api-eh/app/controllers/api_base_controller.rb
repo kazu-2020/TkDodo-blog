7eac166eefa0cf7142a1a6418ff62a567fe51b9e
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-10-25 11:49:06 +0900

    Request_storeのキー名をapi_request_urlsに修正

--- api-eh/app/controllers/api_base_controller.rb
+++ api-eh/app/controllers/api_base_controller.rb
@@ -2,6 +2,7 @@ class ApiBaseController < ApplicationController
   # レスポンスヘッダにX-API-URLを設定するため、各コントローラーで呼び出してください
   # https://github.com/d7lab/aw-editorialhands/issues/1535
   def set_x_api_url_to_header
-    response.headers['X-Api-Url'] = (RequestStore.store[:url].join(',') if RequestStore.store[:url].present?)
+    response.headers['X-Api-Url'] =
+      (RequestStore.store[:api_request_urls].join(',') if RequestStore.store[:api_request_urls].present?)
   end
 end
