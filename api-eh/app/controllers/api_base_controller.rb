9cd23b2c108599920c0da7a37da080a260eada31
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-10-27 14:52:35 +0900

    dev環境でx-api-urlが表示されない不具合の修正

--- api-eh/app/controllers/api_base_controller.rb
+++ api-eh/app/controllers/api_base_controller.rb
@@ -4,7 +4,11 @@ class ApiBaseController < ApplicationController
   # リクエストごとに呼び出されるclientのURLをX-API-URLとしてレスポンスヘッダに設定しています
   # https://github.com/d7lab/aw-editorialhands/issues/1535
   def set_x_api_url_to_header
-    response.headers['X-Api-Url'] =
-      (RequestStore.store[:api_request_urls].join(',') if RequestStore.store[:api_request_urls].present?)
+    response.headers['x-api-url'] =
+      if RequestStore.store[:api_request_urls].present?
+        RequestStore.store[:api_request_urls].join(',')
+      else
+        ''
+      end
   end
 end
