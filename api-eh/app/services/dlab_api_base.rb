4c438ef1fa8e49d63495aaad2b48b4249bf219cd
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-10-27 16:38:28 +0900

    x-api-urlの不具合対応の際に設置したputsメソッドが残っていたため削除

--- api-eh/app/services/dlab_api_base.rb
+++ api-eh/app/services/dlab_api_base.rb
@@ -22,7 +22,6 @@ class DlabApiBase
   def handle_response(response) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/AbcSize
     case response.status
     when 200..299
-      puts "my-debbug: request_url : #{response.env.url}"
       add_url_to_request_store(response.env.url)
       JSON.parse(response.body, symbolize_names: true)
     when 400 then raise BadRequest
