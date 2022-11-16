764fc2adda6222c7834eded851c4f3d2b0fe85aa
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-11-16 11:50:50 +0900

    フォーマットの修正

--- api-eh/spec/models/series_deck_spec.rb
+++ api-eh/spec/models/series_deck_spec.rb
@@ -9,6 +9,7 @@ describe 'SeriesDeck' do
     it 'playlistの紐付けが正しく行われること' do
       new_playlist_ids = series_deck.series_playlists.map(&:series_id)
       series_deck.rebuild_playlists_to(new_playlist_ids)
+
       expect(series_deck.reload.series_playlists.map(&:series_id)).to eq new_playlist_ids
     end
 
@@ -16,6 +17,7 @@ describe 'SeriesDeck' do
       current_playlist_ids = series_deck.series_playlists.map(&:series_id)
       new_playlist_ids = series_deck.series_playlists.map(&:series_id).reverse
       series_deck.rebuild_playlists_to(new_playlist_ids)
+
       expect(series_deck.reload.series_playlists.map(&:series_id)).to eq current_playlist_ids.reverse
     end
   end
