b84b377677f5d83000b99e61ae1373d72da4ed12
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-12-01 16:12:53 +0900

    loading画像を追加

--- api-eh/config/initializers/constants.rb
+++ api-eh/config/initializers/constants.rb
@@ -1,5 +1,5 @@
 module Constants
-  TMP_LOGO_IMG_UPLOADING = '/public/dummy/default10/default10-logo.png'.freeze
-  TMP_EYECATCH_IMG_UPLOADING = '/public/dummy/default10/default10-eyecatch.png'.freeze
-  TMP_HERO_IMG_UPLOADING = '/public/dummy/default10/default10-hero.png'.freeze
+  TMP_LOGO_IMG_UPLOADING = '/public/loading-logo.png'.freeze
+  TMP_EYECATCH_IMG_UPLOADING = '/public/loading-eyecatch.png'.freeze
+  TMP_HERO_IMG_UPLOADING = '/public/loading-hero.png'.freeze
 end
