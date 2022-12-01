fc5e2c951b202b654bce82ae65f3580b5b001bf1
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-12-01 15:27:30 +0900

    storage_keyがcacheの時一時的に別画像のパスを返すように修正

---
+++ api-eh/config/initializers/constants.rb
@@ -0,0 +1,5 @@
+module Constants
+  TMP_LOGO_IMG_UPLOADING = '/public/dummy/default10/default10-logo.png'.freeze
+  TMP_EYECATCH_IMG_UPLOADING = '/public/dummy/default10/default10-eyecatch.png'.freeze
+  TMP_HERO_IMG_UPLOADING = '/public/dummy/default10/default10-hero.png'.freeze
+end
