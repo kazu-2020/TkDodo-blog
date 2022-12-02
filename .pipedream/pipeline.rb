238c4ec4159730c62f9b55922678c6306571a59f
norikt <norikt@gmail.com>
2022-12-02 10:55:36 +0900

    source_nameを明記

--- .pipedream/pipeline.rb
+++ .pipedream/pipeline.rb
@@ -5,6 +5,7 @@ target_branch = ENV['PIPELINE_TARGET_BRANCH'] || 'develop'
 
 stage 'Source' do
   github(
+    source_name: 'Source',
     source: 'd7lab/aw-editorialhands',
     branch: target_branch, # branch defaults to 'master' or the `pipe deploy --branch` option
     auth_token: ssm('/editorialhands/codepipeline/github/token')
@@ -13,8 +14,8 @@ end
 
 stage 'Build-and-Deploy' do
   builds = []
-  builds << { name: "api-eh-#{pipe_env}", project_name: "api-eh-#{pipe_env}" }
-  builds << { name: "editorialhands-frontend-#{pipe_env}", project_name: "editorialhands-frontend-#{pipe_env}" }
+  builds << { name: "api-eh-#{pipe_env}", project_name: "api-eh-#{pipe_env}", input_artifacts: [name: "SourceArtifact"] }
+  builds << { name: "editorialhands-frontend-#{pipe_env}", project_name: "editorialhands-frontend-#{pipe_env}", input_artifacts: [name: "SourceArtifact"] }
 
   codebuild(*builds)
 end
