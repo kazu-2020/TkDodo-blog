06d4a73d796def0bed6255f9c33f12dc1d8f213f
Eigoro Yamamura <yamamura@sikmi.com>
2022-12-14 15:18:13 +0900

    devtoolsのパスを変更

--- frontend-edge/src/providers/app.tsx
+++ frontend-edge/src/providers/app.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ErrorBoundary } from 'react-error-boundary'
 import * as React from 'react'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { CircularProgress } from '@chakra-ui/react'
 
