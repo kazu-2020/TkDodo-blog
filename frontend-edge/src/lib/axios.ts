5427c46a8b0b88954df82f8c5110e54c4047fa34
norikt <norikt@gmail.com>
2022-12-19 14:02:48 +0900

    APIのリクエスト時にheaderにアクセストークンを設定する

--- frontend-edge/src/lib/axios.ts
+++ frontend-edge/src/lib/axios.ts
@@ -1,8 +1,26 @@
-import Axios from 'axios'
+import Axios, { AxiosRequestConfig } from 'axios'
+import { OktaAuth } from '@okta/okta-auth-js'
 
-import { API_BASE_URL } from '@/config'
+import { API_BASE_URL, OIDC_CONFIG } from '@/config'
+
+function authRequestInterceptor(config: AxiosRequestConfig) {
+  const oktaAuth = new OktaAuth(OIDC_CONFIG)
+  const accessToken = oktaAuth.getAccessToken()
+
+  return {
+    ...config,
+    headers: {
+      ...config.headers,
+      authorization: `${accessToken}`,
+      Accept: 'application/json'
+    }
+  }
+}
 
 const axios = Axios.create({
   baseURL: API_BASE_URL
 })
+
+axios.interceptors.request.use(authRequestInterceptor)
+
 export default axios
