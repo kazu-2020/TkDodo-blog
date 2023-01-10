0e2626db1a2ad1a0f2be670c8252382be2fc0f6c
kazu <64774307+kazu-2020@users.noreply.github.com>
2023-01-10 18:43:35 +0900

    :+1: announcement モデルにバリデーション追加

--- api-eh/spec/models/announcement_spec.rb
+++ api-eh/spec/models/announcement_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Announcement do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    subject { Announcement.new(params) }
+    let(:params) {{ contents: contents }}
+
+    context 'contentsがnilの場合' do
+      let(:contents) { nil }
+
+      it { is_expected.to be_invalid }
+    end
+
+    context 'contentsが空文字の場合' do
+      let(:contents) { '' }
+
+      it { is_expected.to be_invalid }
+    end
+  end
 end
