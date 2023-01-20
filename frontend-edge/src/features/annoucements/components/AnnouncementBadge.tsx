b3af4830eccff48d9537d7ebd149f10304779afa
kazu <64774307+kazu-2020@users.noreply.github.com>
2023-01-20 15:06:52 +0900

    :recycle: 他のコンポーネントで使用するのでutilに切り出した

--- frontend-edge/src/features/annoucements/components/AnnouncementBadge.tsx
+++ frontend-edge/src/features/annoucements/components/AnnouncementBadge.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react'
 import { Badge } from '@chakra-ui/react'
 
 import { AnnouncementStatus } from '@/types/announcement'
+import { convertAnnouncementStatus } from '../utils/convertAnnouncementStatus'
 
 type AnnouncementBadgeProps = {
   status: AnnouncementStatus
@@ -26,28 +27,12 @@ const badgeColor = (status: AnnouncementStatus) => {
   }
 }
 
-const badgeStatement = (status: AnnouncementStatus) => {
-  switch (status) {
-    case 'general':
-      return 'お知らせ'
-    case 'improved':
-      return '機能改善'
-    case 'maintenance':
-      return 'メンテナンス情報'
-    case 'attentive':
-      return '注意'
-    case 'emergency':
-      return '緊急'
-    default: {
-      const invalidStatus: never = status
-      throw new Error(`${invalidStatus}は不正な値です`)
-    }
-  }
-}
-
 export const AnnouncementBadge = ({ status }: AnnouncementBadgeProps) => {
   const color = useMemo<string>(() => badgeColor(status), [status])
-  const statement = useMemo<string>(() => badgeStatement(status), [status])
+  const statement = useMemo<string>(
+    () => convertAnnouncementStatus(status),
+    [status]
+  )
 
   return (
     <Badge
@@ -60,30 +45,4 @@ export const AnnouncementBadge = ({ status }: AnnouncementBadgeProps) => {
       {statement}
     </Badge>
   )
-}
-
-if (import.meta.vitest) {
-  const { describe, it, expect } = import.meta.vitest
-
-  describe('badgeStatement', () => {
-    it('generalの場合、「お知らせ」を返すこと', () => {
-      expect(badgeStatement('general')).toBe('お知らせ')
-    })
-
-    it('improvedの場合、「機能改善」を返すこと', () => {
-      expect(badgeStatement('improved')).toBe('機能改善')
-    })
-
-    it('maintenanceの場合、「メンテナンス情報」を返すこと', () => {
-      expect(badgeStatement('maintenance')).toBe('メンテナンス情報')
-    })
-
-    it('attentiveの場合、「注意」を返すこと', () => {
-      expect(badgeStatement('attentive')).toBe('注意')
-    })
-
-    it('emergencyの場合、「緊急」を返すこと', () => {
-      expect(badgeStatement('emergency')).toBe('緊急')
-    })
-  })
 }
