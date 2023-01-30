aca1a7b2f62983aa2d5bf057b4a33b8b2e5d4488
kazu <64774307+kazu-2020@users.noreply.github.com>
2023-01-30 18:56:40 +0900

    :recycle: 選択ボックスの共通化

--- e2e/cypress/support/commands.ts
+++ e2e/cypress/support/commands.ts
@@ -26,40 +26,40 @@
 
 // NOTE: 追加したコマンドは e2e/global.d.ts に定義すること
 
-import "@testing-library/cypress/add-commands"
-import "cypress-real-events/support"
-import "cypress-file-upload"
+import '@testing-library/cypress/add-commands';
+import 'cypress-real-events/support';
+import 'cypress-file-upload';
 import {
   playlistInput,
   recommendDeckInput,
   seriesDeckInput,
-} from "../fixtures/formInput"
+} from '../fixtures/formInput';
 
 /*
  * プレイリストの画像を設定する
  */
-Cypress.Commands.add("attachCoverPhoto", (imageType) => {
-  cy.get(`[data-testid="${imageType}-image-wrapper"]`).focused()
+Cypress.Commands.add('attachCoverPhoto', (imageType) => {
+  cy.get(`[data-testid="${imageType}-image-wrapper"]`).focused();
   cy.get(`[data-testid="${imageType}-image-wrapper"]`)
-    .find("button")
+    .find('button')
     .eq(0)
-    .click({ force: true })
+    .click({ force: true });
   cy.get(
     '[data-testid="cropper-image-modal-body"] input[type="file"]'
-  ).attachFile("domo.jpg")
-  cy.get('[data-testid="next-button"]').click()
-  cy.get('[data-testid="next-button"]').click()
-  cy.get('[data-testid="crop-image-button"]').click()
-})
+  ).attachFile('domo.jpg');
+  cy.get('[data-testid="next-button"]').click();
+  cy.get('[data-testid="next-button"]').click();
+  cy.get('[data-testid="crop-image-button"]').click();
+});
 
 Cypress.Commands.add(
-  "paste",
+  'paste',
   {
     prevSubject: true,
   },
   (subject, data) => {
     const pasteEvent = Object.assign(
-      new Event("paste", {
+      new Event('paste', {
         bubbles: true,
         cancelable: true,
       }),
@@ -69,18 +69,18 @@ Cypress.Commands.add(
           types: Object.keys(data),
         },
       }
-    )
-    subject[0].dispatchEvent(pasteEvent)
+    );
+    subject[0].dispatchEvent(pasteEvent);
 
-    return subject
+    return subject;
   }
-)
+);
 
 /*
  * プレイリストを作成する
  */
 Cypress.Commands.add(
-  "createPlaylist",
+  'createPlaylist',
   (overrides = {}, addEpisodeCount = 0) => {
     const {
       aliasId,
@@ -101,308 +101,316 @@ Cypress.Commands.add(
       sameAs,
       themeGenre,
       ...activesFlags
-    } = playlistInput(overrides)
+    } = playlistInput(overrides);
 
-    cy.visit("/")
-    cy.contains("プレイリスト").click()
-    cy.contains("新規作成").click()
+    cy.visit('/');
+    cy.contains('プレイリスト').click();
+    cy.contains('新規作成').click();
 
     if (addEpisodeCount > 0) {
-      cy.contains("リスト(NItemList)").click()
-      cy.get('[data-testid="search-text-input"]').clear().type("{enter}")
+      cy.contains('リスト(NItemList)').click();
+      cy.get('[data-testid="search-text-input"]').clear().type('{enter}');
       for (let i = 0; i < addEpisodeCount; i++) {
-        cy.get('[aria-label="追加"]').first().click()
+        cy.get('[aria-label="追加"]').first().click();
       }
     }
 
-    cy.contains("記事(NArticle)").click()
+    cy.contains('記事(NArticle)').click();
     if (markedHeader) {
-      cy.get('[data-testid="markedHeader"]').clear().type(markedHeader)
+      cy.get('[data-testid="markedHeader"]').clear().type(markedHeader);
     }
     if (markedFooter) {
-      cy.get('[data-testid="markedFooter"]').clear().type(markedFooter)
+      cy.get('[data-testid="markedFooter"]').clear().type(markedFooter);
     }
     if (authorName) {
-      cy.get('[data-testid="authorName"]').clear().type(authorName)
+      cy.get('[data-testid="authorName"]').clear().type(authorName);
     }
     if (publisherName) {
-      cy.get('[data-testid="publisherName"]').clear().type(publisherName)
+      cy.get('[data-testid="publisherName"]').clear().type(publisherName);
     }
 
-    cy.contains("基本情報(NSeries)").click()
-    cy.get('[data-testid="name"]').type(name)
+    cy.contains('基本情報(NSeries)').click();
+    cy.get('[data-testid="name"]').type(name);
     if (detailedNameRuby) {
-      cy.get('[data-testid="detailedNameRuby"]').clear().type(detailedNameRuby)
+      cy.get('[data-testid="detailedNameRuby"]').clear().type(detailedNameRuby);
     }
     if (detailedCatch) {
-      cy.get('[data-testid="detailedCatch"]').clear().type(detailedCatch)
+      cy.get('[data-testid="detailedCatch"]').clear().type(detailedCatch);
     }
     if (description) {
-      cy.get('[data-testid="description"]').clear().type(description)
+      cy.get('[data-testid="description"]').clear().type(description);
     }
     if (keywords) {
       cy.get('[data-testid="keywords-input-wrapper"] input[type=text]').type(
         keywords,
         { force: true }
-      )
+      );
     }
     if (hashTags) {
       cy.get('[data-testid="hashtags-input-wrapper"] input[type="text"]').type(
         hashTags,
         { force: true }
-      )
+      );
     }
     if (formatGenre) {
-      cy.get('[data-testid="format-genre-wrapper"]').click()
-      cy.contains(formatGenre).click({ force: true })
+      cy.get('#formatGenreCode').type(`${formatGenre}{enter}{enter}`, {
+        force: true,
+      });
     }
     if (themeGenre) {
-      cy.get('[data-testid="theme-genre-wrapper"]').click()
-      cy.contains(themeGenre).click({ force: true })
+      cy.get('#themeGenreCode').type(`${themeGenre}{enter}{enter}`, {
+        force: true,
+      });
     }
     if (sameAs) {
       sameAs.forEach((sameAs, index) => {
-        cy.get('[data-testid="add-same-as-button"]').click()
+        cy.get('[data-testid="add-same-as-button"]').click();
         cy.get(`[data-testid="sameAsAttributes.${index}.name"]`).type(
           sameAs.name
-        )
+        );
         cy.get(`[data-testid="sameAsAttributes.${index}.url"]`).type(
           sameAs.url
-        )
-      })
+        );
+      });
     }
     if (citation) {
       citation.forEach((citation, index) => {
-        cy.get('[data-testid="add-citation-button"]').click()
+        cy.get('[data-testid="add-citation-button"]').click();
         cy.get(`[data-testid="citationsAttributes.${index}.name"]`).type(
           citation.name
-        )
+        );
         cy.get(`[data-testid="citationsAttributes.${index}.url"]`).type(
           citation.url
-        )
-      })
+        );
+      });
     }
     if (aliasId) {
-      cy.get('[data-testid="aliasId"]').clear().type(aliasId)
+      cy.get('[data-testid="aliasId"]').clear().type(aliasId);
     }
 
     if (apiState !== undefined) {
-      const selector = '[data-testid="apiState"] input[type="checkbox"]'
+      const selector = '[data-testid="apiState"] input[type="checkbox"]';
       apiState
         ? cy.get(selector).check({ force: true })
-        : cy.get(selector).uncheck({ force: true })
+        : cy.get(selector).uncheck({ force: true });
     }
 
     // Active ON/OFF
     [
-      "activeItemList",
-      "activeTvepisode",
-      "activeFaqpage",
-      "activeHowto",
-      "activeEvent",
-      "activeRecipe",
-      "activeArticle",
+      'activeItemList',
+      'activeTvepisode',
+      'activeFaqpage',
+      'activeHowto',
+      'activeEvent',
+      'activeRecipe',
+      'activeArticle',
     ].forEach((key) => {
       if (activesFlags[key] !== undefined) {
-        const selector = `[data-testid="${key}"] input[type="checkbox"]`
+        const selector = `[data-testid="${key}"] input[type="checkbox"]`;
         activesFlags[key]
           ? cy.get(selector).check({ force: true })
-          : cy.get(selector).uncheck({ force: true })
+          : cy.get(selector).uncheck({ force: true });
       }
-    })
+    });
 
-    cy.attachCoverPhoto("logo")
-    cy.attachCoverPhoto("eyecatch")
-    cy.attachCoverPhoto("hero")
+    cy.attachCoverPhoto('logo');
+    cy.attachCoverPhoto('eyecatch');
+    cy.attachCoverPhoto('hero');
 
-    beforeSave && beforeSave()
+    beforeSave && beforeSave();
 
-    cy.contains("保存する").click({ force: true })
-    cy.contains("作成しました", { timeout: 10000 })
+    cy.contains('保存する').click({ force: true });
+    cy.contains('作成しました', { timeout: 10000 });
   }
-)
+);
 
 /*
  * プレイリストをすべて削除する
  */
-Cypress.Commands.add("deleteAllPlaylists", () => {
-  cy.visit("/")
-  cy.contains("プレイリスト").click()
-  cy.contains("一覧").click({ force: true })
-  cy.get('[data-testid="api-status-select"]').select("全て")
+Cypress.Commands.add('deleteAllPlaylists', () => {
+  cy.visit('/');
+  cy.contains('プレイリスト').click();
+  cy.contains('一覧').click({ force: true });
+  cy.get('[data-testid="api-status-select"]').select('全て');
 
   // eslint-disable-next-line cypress/no-unnecessary-waiting
-  cy.wait(200) // これを入れないと安定しない
+  cy.wait(200); // これを入れないと安定しない
 
-  cy.get("body").then(($body) => {
+  cy.get('body').then(($body) => {
     if ($body.find('[data-testid="playlist-list-item"]').length) {
       cy.get('[data-testid="playlist-list-item"]').each((el) => {
-        cy.wrap(el).click({ force: true })
+        cy.wrap(el).click({ force: true });
         cy.get('[data-testid="playlist-drawer-delete-button"]').click({
           force: true,
-        })
+        });
         cy.get('[data-testid="playlist-alert-delete-button"]').click({
           force: true,
-        })
-        cy.contains("削除しました")
+        });
+        cy.contains('削除しました');
         // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200)
-      })
+        cy.wait(200);
+      });
     }
-  })
-})
+  });
+});
 
 /*
  * レコメンドデッキを作成する
  */
-Cypress.Commands.add("createRecommendDeck", (overrides = {}, addPlaylistCount = 0) => {
-  const { apiState, beforeSave, description, interfix, name, sameAs } =
-    recommendDeckInput(overrides)
+Cypress.Commands.add(
+  'createRecommendDeck',
+  (overrides = {}, addPlaylistCount = 0) => {
+    const { apiState, beforeSave, description, interfix, name, sameAs } =
+      recommendDeckInput(overrides);
 
-  cy.visit("/")
-  cy.contains("デッキ").click()
-  cy.contains("レコメンドデッキ新規作成").click()
+    cy.visit('/');
+    cy.contains('デッキ').click();
+    cy.contains('レコメンドデッキ新規作成').click();
 
-  if (addPlaylistCount > 0) {
-    cy.contains("リスト(Playlist)").click()
-    cy.get('[data-testid="search-text-input"]').clear().type("{enter}")
-    for (let i = 0; i < addPlaylistCount; i++) {
-      cy.get('[aria-label="追加"]').first().click()
+    if (addPlaylistCount > 0) {
+      cy.contains('リスト(Playlist)').click();
+      cy.get('[data-testid="search-text-input"]').clear().type('{enter}');
+      for (let i = 0; i < addPlaylistCount; i++) {
+        cy.get('[aria-label="追加"]').first().click();
+      }
     }
+
+    cy.contains('基本情報(Deck)').click();
+
+    cy.get('[data-testid="name"]').type(name);
+    cy.get('[data-testid="interfix"]').type(interfix);
+
+    if (description) {
+      cy.get('[data-testid="description"]').type(description);
+    }
+
+    if (apiState) {
+      cy.get('[data-testid="apiState"] input[type="checkbox"]').check({
+        force: true,
+      });
+    }
+
+    if (sameAs) {
+      sameAs.forEach((sameAs, index) => {
+        cy.get('[data-testid="add-same-as-button"]').click();
+        cy.get(`[data-testid="deckSameAsAttributes.${index}.name"]`).type(
+          sameAs.name
+        );
+        cy.get(`[data-testid="deckSameAsAttributes.${index}.url"]`).type(
+          sameAs.url
+        );
+      });
+    }
+
+    beforeSave && beforeSave();
+
+    cy.contains('保存する').click({ force: true });
+    cy.contains('作成しました');
   }
-
-  cy.contains("基本情報(Deck)").click()
-
-  cy.get('[data-testid="name"]').type(name)
-  cy.get('[data-testid="interfix"]').type(interfix)
-
-  if (description) {
-    cy.get('[data-testid="description"]').type(description)
-  }
-
-  if (apiState) {
-    cy.get('[data-testid="apiState"] input[type="checkbox"]').check({
-      force: true,
-    })
-  }
-
-  if (sameAs) {
-    sameAs.forEach((sameAs, index) => {
-      cy.get('[data-testid="add-same-as-button"]').click()
-      cy.get(`[data-testid="deckSameAsAttributes.${index}.name"]`).type(
-        sameAs.name
-      )
-      cy.get(`[data-testid="deckSameAsAttributes.${index}.url"]`).type(
-        sameAs.url
-      )
-    })
-  }
-
-  beforeSave && beforeSave()
-
-  cy.contains("保存する").click({ force: true })
-  cy.contains("作成しました")
-})
+);
 
 /*
  * レコメンドデッキをすべて削除する
  */
-Cypress.Commands.add("deleteAllRecommendDeck", () => {
-  cy.visit("/")
-  cy.contains("レコメンドデッキ一覧").click({ force: true })
+Cypress.Commands.add('deleteAllRecommendDeck', () => {
+  cy.visit('/');
+  cy.contains('レコメンドデッキ一覧').click({ force: true });
 
-  cy.get('[data-testid="api-status-select"]').select("全て", { force: true })
+  cy.get('[data-testid="api-status-select"]').select('全て', { force: true });
 
   // eslint-disable-next-line cypress/no-unnecessary-waiting
-  cy.wait(200) // これを入れないと安定しない
+  cy.wait(200); // これを入れないと安定しない
 
-  cy.get("body").then(($body) => {
+  cy.get('body').then(($body) => {
     if ($body.find('[data-testid="recommend-deck-list-item"]').length) {
       cy.get('[data-testid="recommend-deck-list-item"]').each((el) => {
-        cy.wrap(el).click({ force: true })
+        cy.wrap(el).click({ force: true });
         cy.get('[data-testid="recommend-deck-drawer-delete-button"]').click({
           force: true,
-        })
+        });
         cy.get('[data-testid="recommend-deck-alert-delete-button"]').click({
           force: true,
-        })
-        cy.contains("削除しました")
+        });
+        cy.contains('削除しました');
         // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200)
-      })
+        cy.wait(200);
+      });
     }
-  })
-})
+  });
+});
 
 /*
  * シリーズデッキを作成する
  */
-Cypress.Commands.add("createSeriesDeck", (overrides = {}, addPlaylistCount = 0) => {
-  const { beforeSave, apiState, interfix, description, name } =
-    seriesDeckInput(overrides)
+Cypress.Commands.add(
+  'createSeriesDeck',
+  (overrides = {}, addPlaylistCount = 0) => {
+    const { beforeSave, apiState, interfix, description, name } =
+      seriesDeckInput(overrides);
 
-  cy.visit("/")
-  cy.contains("デッキ").click()
-  cy.contains("シリーズデッキ新規作成").click()
+    cy.visit('/');
+    cy.contains('デッキ').click();
+    cy.contains('シリーズデッキ新規作成').click();
 
-  if (addPlaylistCount > 0) {
-    cy.contains("リスト(Playlist)").click()
-    cy.get('[data-testid="search-text-input"]').clear().type("{enter}")
-    for (let i = 0; i < addPlaylistCount; i++) {
-      cy.get('[aria-label="追加"]').first().click()
+    if (addPlaylistCount > 0) {
+      cy.contains('リスト(Playlist)').click();
+      cy.get('[data-testid="search-text-input"]').clear().type('{enter}');
+      for (let i = 0; i < addPlaylistCount; i++) {
+        cy.get('[aria-label="追加"]').first().click();
+      }
     }
+
+    cy.contains('基本情報(Deck)').click();
+
+    cy.get('[data-testid="name"]').type(name);
+    cy.get('[data-testid="interfix"]').type(interfix);
+
+    if (description) {
+      cy.get('[data-testid="description"]').type(description);
+    }
+
+    if (apiState) {
+      cy.get('[data-testid="apiState"] input[type="checkbox"]').check({
+        force: true,
+      });
+    }
+
+    beforeSave && beforeSave();
+
+    cy.contains('保存する').click({ force: true });
+    cy.contains('作成しました');
   }
-
-  cy.contains("基本情報(Deck)").click()
-
-  cy.get('[data-testid="name"]').type(name)
-  cy.get('[data-testid="interfix"]').type(interfix)
-
-  if (description) {
-    cy.get('[data-testid="description"]').type(description)
-  }
-
-  if (apiState) {
-    cy.get('[data-testid="apiState"] input[type="checkbox"]').check({
-      force: true,
-    })
-  }
-
-  beforeSave && beforeSave()
-
-  cy.contains("保存する").click({ force: true })
-  cy.contains("作成しました")
-})
+);
 
 /*
  * シリーズデッキをすべて削除する
  */
-Cypress.Commands.add("deleteAllSeriesDeck", () => {
-  cy.visit("/")
-  cy.contains("シリーズデッキ一覧").click({ force: true })
+Cypress.Commands.add('deleteAllSeriesDeck', () => {
+  cy.visit('/');
+  cy.contains('シリーズデッキ一覧').click({ force: true });
 
-  cy.get('[data-testid="api-status-select"]').select("全て", { force: true })
+  cy.get('[data-testid="api-status-select"]').select('全て', { force: true });
 
   // eslint-disable-next-line cypress/no-unnecessary-waiting
-  cy.wait(500) // これを入れないと安定しない
+  cy.wait(500); // これを入れないと安定しない
 
-  cy.get("body").then(($body) => {
+  cy.get('body').then(($body) => {
     if ($body.find('[data-testid="series-deck-list-item"]').length) {
       cy.get('[data-testid="series-deck-list-item"]').each((el) => {
-        cy.wrap(el).click({ force: true })
+        cy.wrap(el).click({ force: true });
         cy.get('[data-testid="series-deck-drawer-delete-button"]').click({
           force: true,
-        })
+        });
         cy.get('[data-testid="series-deck-alert-delete-button"]').click({
           force: true,
-        })
-        cy.contains("削除しました")
+        });
+        cy.contains('削除しました');
         // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(200)
-      })
+        cy.wait(200);
+      });
     }
-  })
-})
+  });
+});
 
 /*
  * お知らせを作成する
