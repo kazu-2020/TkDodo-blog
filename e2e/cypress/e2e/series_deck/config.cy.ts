55718d2b49fa0081626fdb391fe3091fab3bf98d
kazu <64774307+kazu-2020@users.noreply.github.com>
2023-01-30 19:22:36 +0900

    :recycle: 入力フォームを共通コンポーネントに置き換え

--- e2e/cypress/e2e/series_deck/config.cy.ts
+++ e2e/cypress/e2e/series_deck/config.cy.ts
@@ -1,31 +1,31 @@
-describe("シリーズデッキ管理設定", () => {
+describe('シリーズデッキ管理設定', () => {
   before(() => {
-    cy.createSeriesDeck()
-  })
+    cy.createSeriesDeck();
+  });
 
-  it("管理メモが更新できること", () => {
-    cy.visit("/")
-    cy.contains("デッキ").click()
-    cy.contains("シリーズデッキ一覧").click()
+  it('管理メモが更新できること', () => {
+    cy.visit('/');
+    cy.contains('デッキ').click();
+    cy.contains('シリーズデッキ一覧').click();
 
-    cy.get('[data-testid="api-status-select"]').select("全て", { force: true })
+    cy.get('[data-testid="api-status-select"]').select('全て', { force: true });
     cy.get('[data-testid="series-deck-list-item"]')
       .first()
-      .click({ force: true })
-    cy.contains('管理設定').click({ force: true })
+      .click({ force: true });
+    cy.contains('管理設定').click({ force: true });
 
-    cy.get('[data-testid="adminMemo"]').clear().type('管理メモメモ')
+    cy.get('#adminMemo').clear().type('管理メモメモ');
 
-    cy.contains("保存する").click({ force: true })
-    cy.contains("保存しました", { timeout: 10000 })
+    cy.contains('保存する').click({ force: true });
+    cy.contains('保存しました', { timeout: 10000 });
 
     // 登録内容の確認
-    cy.visit("/")
-    cy.contains("デッキ").click()
-    cy.contains("シリーズデッキ一覧").click()
+    cy.visit('/');
+    cy.contains('デッキ').click();
+    cy.contains('シリーズデッキ一覧').click();
 
-    cy.get('[data-testid="api-status-select"]').select("全て", { force: true })
+    cy.get('[data-testid="api-status-select"]').select('全て', { force: true });
 
-    cy.contains('管理メモメモ')
-  })
-})
+    cy.contains('管理メモメモ');
+  });
+});
