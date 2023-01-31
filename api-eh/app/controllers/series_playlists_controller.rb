c3d38d4a21577a1aa65b868cf2c16f8e29038816
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-01-31 09:48:00 +0900

    series_playlistに認可の制御を実装

--- api-eh/app/controllers/series_playlists_controller.rb
+++ api-eh/app/controllers/series_playlists_controller.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class SeriesPlaylistsController < ApiBaseController
+  authorize_resource
+
   def episodes
     @series_playlist = SeriesPlaylist.find(params[:id])
+    ability.authorize! :episodes, @series_playlist
   end
 
   def search
     @result = SearchSeriesPlaylist.new.call(DlabApiClient.new, search_params)
+    ability.authorize! :search, SeriesPlaylist
   end
 
   private
