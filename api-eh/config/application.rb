442b08eb9633b799e86d4234c63fbb900130e287
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-01 11:52:45 +0900

    application.ymlをokta.ymlに命名修正

--- api-eh/config/application.rb
+++ api-eh/config/application.rb
@@ -53,7 +53,7 @@ module EditorialHandsAPI
     end
 
     config.before_configuration do
-      env_file = Rails.root.join('config', 'application.yml')
+      env_file = Rails.root.join('config', 'okta.yml')
       return unless File.exist?(env_file)
 
       yaml = YAML.safe_load(File.open(env_file))
