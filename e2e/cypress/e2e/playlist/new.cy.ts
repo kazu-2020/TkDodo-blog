caa7cc3ed3d58190115039687d5273c5097baf24
kazu <64774307+kazu-2020@users.noreply.github.com>
2023-02-01 11:11:40 +0900

    :shirt: Lint修正を元に戻した

--- e2e/cypress/e2e/playlist/new.cy.ts
+++ e2e/cypress/e2e/playlist/new.cy.ts
@@ -1,39 +1,39 @@
-import { playlistInput } from '../../fixtures/formInput'
+import { playlistInput } from "../../fixtures/formInput"
 
-describe('プレイリスト新規作成', () => {
+describe("プレイリスト新規作成", () => {
   before(() => {
     cy.deleteAllPlaylists()
   })
 
-  context('全項目を入力した場合', () => {
+  context("全項目を入力した場合", () => {
     const inputData = playlistInput({
-      name: 'テストプレイリスト',
-      detailedCatch: 'キャッチコピーはこちら',
-      detailedNameRuby: 'てすとぷれいりすと',
-      description: '説明',
-      keywords: 'キーワード1{enter}キーワード2{enter}',
-      hashTags: '#ハッシュタグ1{enter}',
-      formatGenre: '報道',
-      themeGenre: '科学',
-      sameAs: [{ name: 'same-as', url: 'https://example.com' }],
-      citation: [{ name: 'citation', url: 'https://example.com' }],
-      aliasId: 'alias-123456789',
-      markedHeader: 'ヘッダー',
-      markedFooter: 'フッター',
-      authorName: '著者',
-      publisherName: '出版社',
+      name: "テストプレイリスト",
+      detailedCatch: "キャッチコピーはこちら",
+      detailedNameRuby: "てすとぷれいりすと",
+      description: "説明",
+      keywords: "キーワード1{enter}キーワード2{enter}",
+      hashTags: "#ハッシュタグ1{enter}",
+      formatGenre: "報道",
+      themeGenre: "科学",
+      sameAs: [{ name: "same-as", url: "https://example.com" }],
+      citation: [{ name: "citation", url: "https://example.com" }],
+      aliasId: "alias-123456789",
+      markedHeader: "ヘッダー",
+      markedFooter: "フッター",
+      authorName: "著者",
+      publisherName: "出版社",
       activeArticle: true,
       beforeSave: () => {
-        cy.contains('記事(NArticle)').click()
+        cy.contains("記事(NArticle)").click()
         // paragraph
-        cy.get('#editorjs').get('div.ce-block').click().type('test{enter}')
+        cy.get("#editorjs").get("div.ce-block").click().type("test{enter}")
         // 画像をペースト
-        cy.get('.ce-paragraph').last().paste({
-          'text/plain': 'https://picsum.photos/200.jpg',
+        cy.get(".ce-paragraph").last().paste({
+          "text/plain": "https://picsum.photos/200.jpg",
         })
-        cy.get('.image-tool__image-picture')
-          .should('have.attr', 'src')
-          .should('include', 'article_images')
+        cy.get(".image-tool__image-picture")
+          .should("have.attr", "src")
+          .should("include", "article_images")
 
         cy.wait(500) // 記事画像を貼り付けてすぐ保存ボタンを押すと保存されない場合があるため
       },
@@ -43,13 +43,13 @@ describe('プレイリスト新規作成', () => {
       cy.createPlaylist(inputData, 2)
     })
 
-    it('正常にプレイリストが作成されること', () => {
+    it("正常にプレイリストが作成されること", () => {
       // 作成したプレイリストの確認 ------------------------------------------------
-      cy.visit('/')
-      cy.contains('プレイリスト').click()
-      cy.contains('一覧').click({ force: true })
+      cy.visit("/")
+      cy.contains("プレイリスト").click()
+      cy.contains("一覧").click({ force: true })
 
-      cy.get('[data-testid="api-status-select"]').select('全て')
+      cy.get('[data-testid="api-status-select"]').select("全て")
 
       cy.get('[data-testid="playlist-list-items"]')
         .contains(inputData.name)
@@ -58,69 +58,69 @@ describe('プレイリスト新規作成', () => {
       // ドロワーの内容チェック
       const drawerSelector = '[data-testid="playlist-drawer-info"]'
       cy.get(drawerSelector).contains(inputData.name)
-      cy.get(drawerSelector).contains('API公開中')
-      cy.get(drawerSelector).contains('summary')
-      cy.get(drawerSelector).contains('キャッチコピーはこちら')
-      cy.get(drawerSelector).contains('説明')
+      cy.get(drawerSelector).contains("API公開中")
+      cy.get(drawerSelector).contains("summary")
+      cy.get(drawerSelector).contains("キャッチコピーはこちら")
+      cy.get(drawerSelector).contains("説明")
       cy.get(`[data-testid="playlist-drawer-info__images"] img`).should(
-        'have.lengthOf',
+        "have.lengthOf",
         3
       )
       cy.get('[data-testid="playlist-drawer-episode-list__item"]').should(
-        'have.lengthOf',
+        "have.lengthOf",
         2
       )
       cy.get('[data-testid="playlist-drawer-edit-button"]').click()
 
       // プレイリスト編集画面の内容チェック
       // リスト(NItemList) タブ
-      cy.contains('リスト(NItemList)').click()
+      cy.contains("リスト(NItemList)").click()
       cy.get('[data-testid="edit-episode-list__item"]').should(
-        'have.lengthOf',
+        "have.lengthOf",
         2
       )
 
       // 記事(NArticle) タブ
-      cy.contains('記事(NArticle)').click()
+      cy.contains("記事(NArticle)").click()
       cy.get('[data-testid="markedHeader"]').should(
-        'have.value',
+        "have.value",
         inputData.markedHeader
       )
-      cy.get('.ce-paragraph').contains('test')
-      cy.get('.image-tool__image-picture').should('have.lengthOf', 1)
+      cy.get(".ce-paragraph").contains("test")
+      cy.get(".image-tool__image-picture").should("have.lengthOf", 1)
       cy.get('[data-testid="markedFooter"]').should(
-        'have.value',
+        "have.value",
         inputData.markedFooter
       )
       cy.get('[data-testid="authorName"]').should(
-        'have.value',
+        "have.value",
         inputData.authorName
       )
       cy.get('[data-testid="publisherName"]').should(
-        'have.value',
+        "have.value",
         inputData.publisherName
       )
 
       // 基本情報(NSeries) タブ
-      cy.contains('基本情報(NSeries)').click()
-      cy.get('[data-testid="name"]').should('have.value', inputData.name)
+      cy.contains("基本情報(NSeries)").click()
+      cy.get('[data-testid="name"]').should("have.value", inputData.name)
       cy.get('[data-testid="detailedNameRuby"]').should(
-        'have.value',
+        "have.value",
         inputData.detailedNameRuby
       )
       cy.get('[data-testid="detailedCatch"]').should(
-        'have.value',
+        "have.value",
         inputData.detailedCatch
       )
       cy.get('[data-testid="description"]').should(
-        'have.value',
+        "have.value",
         inputData.description
       )
-      cy.get('[data-testid="keywords-input-wrapper"]').contains('キーワード1')
-      cy.get('[data-testid="keywords-input-wrapper"]').contains('キーワード2')
+      cy.get('[data-testid="keywords-input-wrapper"]').contains("キーワード1")
+      cy.get('[data-testid="keywords-input-wrapper"]').contains("キーワード2")
 
       cy.get('[data-testid="hashtags-input-wrapper"]').contains(
-        '#ハッシュタグ1'
+        "#ハッシュタグ1"
       )
 
       cy.get('[data-testid="select-formatGenreCode"]').contains(
@@ -131,62 +131,62 @@ describe('プレイリスト新規作成', () => {
       )
       // 画像
       cy.get(`[data-testid="logo-image-wrapper"] img`).should(
-        'not.have.attr',
-        'src',
-        'dummy'
+        "not.have.attr",
+        "src",
+        "dummy"
       )
       cy.get(`[data-testid="eyecatch-image-wrapper"] img`).should(
-        'not.have.attr',
-        'src',
-        'dummy'
+        "not.have.attr",
+        "src",
+        "dummy"
       )
       cy.get(`[data-testid="hero-image-wrapper"] img`).should(
-        'not.have.attr',
-        'src',
-        'dummy'
+        "not.have.attr",
+        "src",
+        "dummy"
       )
       // color
       cy.get('[data-testid="adjusted-color-text-primaryLight"]').contains(
-        '#84919e'
+        "#84919e"
       )
       cy.get('[data-testid="adjusted-color-text-primaryDark"]').contains(
-        '#84919e'
+        "#84919e"
       )
       cy.get('[data-testid="adjusted-color-text-linkLight"]').contains(
-        '#6a757f'
+        "#6a757f"
       )
       cy.get('[data-testid="adjusted-color-text-linkDark"]').contains(
-        '#84919e'
+        "#84919e"
       )
       // SameAs
       cy.get('[data-testid="sameAsAttributes.0.name"]').should(
-        'have.value',
+        "have.value",
         inputData.sameAs[0].name
       )
       cy.get('[data-testid="sameAsAttributes.0.url"]').should(
-        'have.value',
+        "have.value",
         inputData.sameAs[0].url
       )
       // Citation
       cy.get('[data-testid="citationsAttributes.0.name"]').should(
-        'have.value',
+        "have.value",
         inputData.citation[0].name
       )
       cy.get('[data-testid="citationsAttributes.0.url"]').should(
-        'have.value',
+        "have.value",
         inputData.citation[0].url
       )
       // エイリアスID
-      cy.get('[data-testid="aliasId"]').should('have.value', inputData.aliasId)
+      cy.get('[data-testid="aliasId"]').should("have.value", inputData.aliasId)
       // API State
       cy.get('[data-testid="apiState"] input[type="checkbox"]').should(
-        'be.checked'
+        "be.checked"
       )
       cy.get('[data-testid="activeTvepisode"] input[type="checkbox"]').should(
-        'be.checked'
+        "be.checked"
       )
       cy.get('[data-testid="activeArticle"] input[type="checkbox"]').should(
-        'be.checked'
+        "be.checked"
       )
     })
   })
--- e2e/cypress/e2e/playlist/update.cy.ts
+++ e2e/cypress/e2e/playlist/update.cy.ts
@@ -1,44 +1,44 @@
-import { playlistInput } from '../../fixtures/formInput'
+import { playlistInput } from "../../fixtures/formInput"
 
-describe('プレイリスト更新', () => {
+describe("プレイリスト更新", () => {
   before(() => {
     cy.deleteAllPlaylists()
-    cy.createPlaylist({ name: 'テストプレイリスト' }, 2)
+    cy.createPlaylist({ name: "テストプレイリスト" }, 2)
   })
 
-  context('全項目を更新した場合', () => {
+  context("全項目を更新した場合", () => {
     const inputData = playlistInput({
-      name: 'テストプレイリスト2',
-      detailedCatch: 'キャッチコピーはこちら2',
-      detailedNameRuby: 'てすとぷれいりすと2',
-      description: '説明2',
-      keywords: 'キーワード3{enter}',
-      hashTags: '#ハッシュタグ2{enter}',
-      formatGenre: 'ドキュメンタリー',
-      themeGenre: '音楽全般',
-      sameAs: [{ name: 'same-as2', url: 'https://example2.com' }],
-      citation: [{ name: 'citation2', url: 'https://example2.com' }],
-      aliasId: 'alias-0000',
-      markedHeader: 'ヘッダー2',
-      markedFooter: 'フッター2',
-      authorName: 'author',
-      publisherName: 'publisher',
+      name: "テストプレイリスト2",
+      detailedCatch: "キャッチコピーはこちら2",
+      detailedNameRuby: "てすとぷれいりすと2",
+      description: "説明2",
+      keywords: "キーワード3{enter}",
+      hashTags: "#ハッシュタグ2{enter}",
+      formatGenre: "ドキュメンタリー",
+      themeGenre: "音楽全般",
+      sameAs: [{ name: "same-as2", url: "https://example2.com" }],
+      citation: [{ name: "citation2", url: "https://example2.com" }],
+      aliasId: "alias-0000",
+      markedHeader: "ヘッダー2",
+      markedFooter: "フッター2",
+      authorName: "author",
+      publisherName: "publisher",
     })
 
     before(() => {
-      cy.visit('/')
-      cy.contains('プレイリスト').click()
-      cy.contains('一覧').click({ force: true })
+      cy.visit("/")
+      cy.contains("プレイリスト").click()
+      cy.contains("一覧").click({ force: true })
 
-      cy.get('[data-testid="api-status-select"]').select('全て')
+      cy.get('[data-testid="api-status-select"]').select("全て")
 
       cy.get('[data-testid="playlist-list-items"]')
-        .contains('テストプレイリスト')
+        .contains("テストプレイリスト")
         .click()
       cy.get('[data-testid="playlist-drawer-edit-button"]').click()
 
       // リスト(NItemList)の編集 ------------------------------------------------
-      cy.contains('リスト(NItemList)').click()
+      cy.contains("リスト(NItemList)").click()
 
       // エピソードを1件削除
       cy.get('[data-testid="edit-episode-list"] [aria-label="削除"]')
@@ -46,25 +46,25 @@ describe('プレイリスト更新', () => {
         .click()
 
       cy.get('[data-testid="edit-episode-list"]')
-        .contains('プレイリストを追加してください')
-        .should('not.exist')
+        .contains("プレイリストを追加してください")
+        .should("not.exist")
 
       // 記事(NArticle)の編集 --------------------------------------------------
-      cy.contains('記事(NArticle)').click()
+      cy.contains("記事(NArticle)").click()
 
       cy.get('[data-testid="markedHeader"]')
         .clear()
         .type(inputData.markedHeader)
 
       // paragraph
-      cy.get('#editorjs').get('div.ce-block').click().type('test{enter}')
+      cy.get("#editorjs").get("div.ce-block").click().type("test{enter}")
       // 画像をペースト
-      cy.get('.ce-paragraph').last().paste({
-        'text/plain': 'https://picsum.photos/200.jpg',
+      cy.get(".ce-paragraph").last().paste({
+        "text/plain": "https://picsum.photos/200.jpg",
       })
-      cy.get('.image-tool__image-picture')
-        .should('have.attr', 'src')
-        .should('include', 'article_images')
+      cy.get(".image-tool__image-picture")
+        .should("have.attr", "src")
+        .should("include", "article_images")
 
       cy.get('[data-testid="markedFooter"]')
         .clear()
@@ -72,23 +72,23 @@ describe('プレイリスト更新', () => {
 
       // 著者の変更
       cy.get('[data-testid="authorType-radio-group"]')
-        .contains('個人(Person)')
+        .contains("個人(Person)")
         .click()
       cy.get('[data-testid="authorName"]').clear().type(inputData.authorName)
 
       // 発行者の変更
       cy.get('[data-testid="publisherType-radio-group"]')
-        .contains('個人(Person)')
+        .contains("個人(Person)")
         .click()
       cy.get('[data-testid="publisherName"]')
         .clear()
         .type(inputData.publisherName)
 
       // 基本情報(NSeries)の編集 ------------------------------------------------
-      cy.contains('基本情報(NSeries)').click()
+      cy.contains("基本情報(NSeries)").click()
 
       // 基本情報(NSeries) タブ
-      cy.contains('基本情報(NSeries)').click()
+      cy.contains("基本情報(NSeries)").click()
       cy.get('[data-testid="name"]').clear().type(inputData.name)
       cy.get('[data-testid="detailedNameRuby"]')
         .clear()
@@ -106,13 +106,13 @@ describe('プレイリスト更新', () => {
         { force: true }
       )
 
-      cy.get('#formatGenreCode').type(
+      cy.get("#formatGenreCode").type(
         `${inputData.formatGenre}{enter}{enter}`,
         {
           force: true,
         }
       )
-      cy.get('#themeGenreCode').type(`${inputData.themeGenre}{enter}{enter}`, {
+      cy.get("#themeGenreCode").type(`${inputData.themeGenre}{enter}{enter}`, {
         force: true,
       })
       inputData.sameAs.forEach((sameAs, index) => {
@@ -139,17 +139,17 @@ describe('プレイリスト更新', () => {
         force: true,
       })
 
-      cy.contains('保存する').click({ force: true })
-      cy.contains('保存しました', { timeout: 10000 })
+      cy.contains("保存する").click({ force: true })
+      cy.contains("保存しました", { timeout: 10000 })
     })
 
-    it('プレイリストの内容が正しいこと', () => {
+    it("プレイリストの内容が正しいこと", () => {
       // 作成したプレイリストの確認 ------------------------------------------------
-      cy.visit('/')
-      cy.contains('プレイリスト').click()
-      cy.contains('一覧').click({ force: true })
+      cy.visit("/")
+      cy.contains("プレイリスト").click()
+      cy.contains("一覧").click({ force: true })
 
-      cy.get('[data-testid="api-status-select"]').select('全て')
+      cy.get('[data-testid="api-status-select"]').select("全て")
 
       cy.get('[data-testid="playlist-list-items"]')
         .contains(inputData.name)
@@ -158,74 +158,74 @@ describe('プレイリスト更新', () => {
       // ドロワーの内容チェック
       const drawerSelector = '[data-testid="playlist-drawer-info"]'
       cy.get(drawerSelector).contains(inputData.name)
-      cy.get(drawerSelector).contains('API公開中')
-      cy.get(drawerSelector).contains('summary')
-      cy.get(drawerSelector).contains('キャッチコピーはこちら')
-      cy.get(drawerSelector).contains('説明')
+      cy.get(drawerSelector).contains("API公開中")
+      cy.get(drawerSelector).contains("summary")
+      cy.get(drawerSelector).contains("キャッチコピーはこちら")
+      cy.get(drawerSelector).contains("説明")
       cy.get(`[data-testid="playlist-drawer-info__images"] img`).should(
-        'have.lengthOf',
+        "have.lengthOf",
         3
       )
       cy.get('[data-testid="playlist-drawer-episode-list__item"]').should(
-        'have.lengthOf',
+        "have.lengthOf",
         1
       )
       cy.get('[data-testid="playlist-drawer-edit-button"]').click()
 
       // プレイリスト編集画面の内容チェック
       // リスト(NItemList) タブ
-      cy.contains('リスト(NItemList)').click()
+      cy.contains("リスト(NItemList)").click()
       cy.get('[data-testid="edit-episode-list__item"]').should(
-        'have.lengthOf',
+        "have.lengthOf",
         1
       )
 
       // 記事(NArticle) タブ
-      cy.contains('記事(NArticle)').click()
+      cy.contains("記事(NArticle)").click()
       cy.get('[data-testid="markedHeader"]').should(
-        'have.value',
+        "have.value",
         inputData.markedHeader
       )
-      cy.get('.ce-paragraph').contains('test')
-      cy.get('.image-tool__image-picture').should('have.lengthOf', 1)
+      cy.get(".ce-paragraph").contains("test")
+      cy.get(".image-tool__image-picture").should("have.lengthOf", 1)
       cy.get('[data-testid="markedFooter"]').should(
-        'have.value',
+        "have.value",
         inputData.markedFooter
       )
       cy.get('[data-testid="radio-authorType-person"]')
         .siblings('input[type="radio"]')
-        .should('be.checked')
+        .should("be.checked")
       cy.get('[data-testid="authorName"]').should(
-        'have.value',
+        "have.value",
         inputData.authorName
       )
       cy.get('[data-testid="radio-publisherType-person"]')
         .siblings('input[type="radio"]')
-        .should('be.checked')
+        .should("be.checked")
       cy.get('[data-testid="publisherName"]').should(
-        'have.value',
+        "have.value",
         inputData.publisherName
       )
 
       // 基本情報(NSeries) タブ
-      cy.contains('基本情報(NSeries)').click()
-      cy.get('[data-testid="name"]').should('have.value', inputData.name)
+      cy.contains("基本情報(NSeries)").click()
+      cy.get('[data-testid="name"]').should("have.value", inputData.name)
       cy.get('[data-testid="detailedNameRuby"]').should(
-        'have.value',
+        "have.value",
         inputData.detailedNameRuby
       )
       cy.get('[data-testid="detailedCatch"]').should(
-        'have.value',
+        "have.value",
         inputData.detailedCatch
       )
       cy.get('[data-testid="description"]').should(
-        'have.value',
+        "have.value",
         inputData.description
       )
-      cy.get('[data-testid="keywords-input-wrapper"]').contains('キーワード3')
+      cy.get('[data-testid="keywords-input-wrapper"]').contains("キーワード3")
 
       cy.get('[data-testid="hashtags-input-wrapper"]').contains(
-        '#ハッシュタグ2'
+        "#ハッシュタグ2"
       )
 
       cy.get('[data-testid="select-formatGenreCode"]').contains(
@@ -236,62 +236,62 @@ describe('プレイリスト更新', () => {
       )
       // 画像
       cy.get(`[data-testid="logo-image-wrapper"] img`).should(
-        'not.have.attr',
-        'src',
-        'dummy'
+        "not.have.attr",
+        "src",
+        "dummy"
       )
       cy.get(`[data-testid="eyecatch-image-wrapper"] img`).should(
-        'not.have.attr',
-        'src',
-        'dummy'
+        "not.have.attr",
+        "src",
+        "dummy"
       )
       cy.get(`[data-testid="hero-image-wrapper"] img`).should(
-        'not.have.attr',
-        'src',
-        'dummy'
+        "not.have.attr",
+        "src",
+        "dummy"
       )
       // color
       cy.get('[data-testid="adjusted-color-text-primaryLight"]').contains(
-        '#84919e'
+        "#84919e"
       )
       cy.get('[data-testid="adjusted-color-text-primaryDark"]').contains(
-        '#84919e'
+        "#84919e"
       )
       cy.get('[data-testid="adjusted-color-text-linkLight"]').contains(
-        '#6a757f'
+        "#6a757f"
       )
       cy.get('[data-testid="adjusted-color-text-linkDark"]').contains(
-        '#84919e'
+        "#84919e"
       )
       // SameAs
       cy.get('[data-testid="sameAsAttributes.0.name"]').should(
-        'have.value',
+        "have.value",
         inputData.sameAs[0].name
       )
       cy.get('[data-testid="sameAsAttributes.0.url"]').should(
-        'have.value',
+        "have.value",
         inputData.sameAs[0].url
       )
       // Citation
       cy.get('[data-testid="citationsAttributes.0.name"]').should(
-        'have.value',
+        "have.value",
         inputData.citation[0].name
       )
       cy.get('[data-testid="citationsAttributes.0.url"]').should(
-        'have.value',
+        "have.value",
         inputData.citation[0].url
       )
       // エイリアスID
-      cy.get('[data-testid="aliasId"]').should('have.value', inputData.aliasId)
+      cy.get('[data-testid="aliasId"]').should("have.value", inputData.aliasId)
       // API State
       cy.get('[data-testid="apiState"] input[type="checkbox"]').should(
-        'be.checked'
+        "be.checked"
       )
       cy.get('[data-testid="activeTvepisode"] input[type="checkbox"]').should(
-        'be.checked'
+        "be.checked"
       )
       cy.get('[data-testid="activeArticle"] input[type="checkbox"]').should(
-        'be.checked'
+        "be.checked"
       )
     })
   })
