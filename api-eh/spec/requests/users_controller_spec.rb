64a8ce70dcb41fedf43d03a9e084eee9400a296f
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-09 19:26:24 +0900

    specの修正

--- api-eh/spec/requests/users_controller_spec.rb
+++ api-eh/spec/requests/users_controller_spec.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 describe 'UsersController' do
   before do
+    user = create(:user, :super_admin)
+    allow_any_instance_of(Secured).to receive(:authenticate_request!).and_return(user)
+    allow_any_instance_of(ApiBaseController).to receive(:current_user).and_return(user)
+
     create_list(:user, 60)
   end
 
@@ -55,6 +57,60 @@ describe 'UsersController' do
           expect(body['users'].length).to eq 0
           expect(response).to have_http_status :ok
         end
+      end
+    end
+
+    context 'ページネーションのテスト' do
+      it 'ページネーションの情報が取得できること' do
+        get users_url, params: { page: 2 }
+        body = JSON.parse(response.body)
+        expect(body['pagination']['totalPages']).to eq 2
+        expect(body['pagination']['currentPage']).to eq 2
+        expect(body['pagination']['nextPage']).to be_nil
+        expect(body['pagination']['previousPage']).to eq 1
+        expect(response).to have_http_status :ok
+      end
+    end
+
+    context 'システムロールによる絞り込み' do
+      before do
+        create(:user, :user_admin, :deck_admin)
+        create(:user, :playlist_admin)
+        create(:user, :deck_admin)
+        create(:user, :reader_user)
+      end
+
+      it 'システムロールがユーザー管理者のユーザーのデータを取得できること' do
+        get users_url, params: { role: 'user_admin' }
+        body = JSON.parse(response.body)
+        # p body['users'][0]['systemRoles'][0]
+        expect(body['users'].length).to eq 1
+        expect(body['users'][0]['systemRoles']).to eq ['userAdmin']
+        expect(response).to have_http_status :ok
+      end
+
+      it 'システムロールがプレイリスト管理者のユーザーのデータを取得できること' do
+        get users_url, params: { role: 'playlist_admin' }
+        body = JSON.parse(response.body)
+        expect(body['users'].length).to eq 1
+        expect(body['users'][0]['systemRole']).to eq 'playlist_admin'
+        expect(response).to have_http_status :ok
+      end
+
+      it 'システムロールがデッキ管理者のユーザーのデータを取得できること' do
+        get users_url, params: { role: 'deck_admin' }
+        body = JSON.parse(response.body)
+        expect(body['users'].length).to eq 1
+        expect(body['users'][0]['systemRole']).to eq 'deck_admin'
+        expect(response).to have_http_status :ok
+      end
+
+      it 'システムロールが閲覧者のユーザーのデータを取得できること' do
+        get users_url, params: { role: 'reader_user' }
+        body = JSON.parse(response.body)
+        expect(body['users'].length).to eq 1
+        expect(body['users'][0]['systemRole']).to eq 'reader_user'
+        expect(response).to have_http_status :ok
       end
     end
   end
