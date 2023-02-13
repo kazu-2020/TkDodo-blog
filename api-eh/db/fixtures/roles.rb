78798ed22da283d6ca6767733903292f0ec5151b
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-13 17:13:31 +0900

    seedの修正

--- api-eh/db/fixtures/roles.rb
+++ api-eh/db/fixtures/roles.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-Role.create(id: 1, name: 'super_admin')
-Role.create(id: 2, name: 'user_admin')
-Role.create(id: 3, name: 'playlist_admin')
-Role.create(id: 4, name: 'deck_admin')
-Role.create(id: 5, name: 'reader_user')
+Ability::SYSTEM_ROLES.keys.map do |role_name|
+  Role.create!(name: role_name)
+end
--- api-eh/db/fixtures/users_roles.rb
+++ api-eh/db/fixtures/users_roles.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-User.all.ids.each do |user_id|
-  Role.all.ids.each do |role_id|
+User.ids.each do |user_id|
+  Role.ids.each do |role_id|
     UsersRole.create(user_id: user_id, role_id: role_id)
   end
 end
