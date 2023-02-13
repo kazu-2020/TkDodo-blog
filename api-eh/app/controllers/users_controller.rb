4f2e5c4d9d1d9a8836d2d23ba9dcdd58f0bd8c72
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-13 18:25:02 +0900

    controllerを修正

--- api-eh/app/controllers/users_controller.rb
+++ api-eh/app/controllers/users_controller.rb
@@ -11,8 +11,11 @@ class UsersController < ApiBaseController
 
   def set_users
     users = User.includes(:roles).recent
-    users = users.keyword_like(users, params[:keyword]) if params[:keyword].present?
-    users = User.joins(:roles).where(roles: { name: params[:role].to_s }) if params[:role].present?
+    users = users.keyword_like(params[:keyword]) if params[:keyword].present?
+    if params[:role].present?
+      users = users.where(roles: { name: params[:role].to_s }) # ロール名でUserを絞り込み
+      users = User.includes(:roles).where(users: { id: users.ids }) # 絞り込んだUserのidに紐づくロールを全て取得
+    end
 
     users
   end
