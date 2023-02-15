856fa85af31f8a9e590782cb8dfcd37fd94c4641
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2023-02-15 10:58:05 +0900

    super_adminを削除

--- api-eh/app/models/concerns/rolable.rb
+++ api-eh/app/models/concerns/rolable.rb
@@ -3,16 +3,6 @@ module Rolable
 
   included do
     rolify
-    after_create :assign_default_role
-  end
-
-  def assign_default_role
-    # FIXME: 適切なロールに修正する
-    add_role(:super_admin) if roles.blank? # seedを作成するまでsuper_adminを割り当てる
-  end
-
-  def grant_super_admin!
-    add_role :super_admin
   end
 
   def grant_user_admin!
